99c10c868c464efc40f56ea78aa8d7def7e5ed55
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-02-04 14:24:18 +0000

    git-svn-id: svn://svn.appwork.org/utils@2238 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/ExtMergedIcon.java
+++ src/org/appwork/swing/components/ExtMergedIcon.java
@@ -11,6 +11,7 @@ import java.util.TreeSet;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
+import org.appwork.utils.Application;
 import org.appwork.utils.ImageProvider.ImageProvider;
 
 public class ExtMergedIcon implements Icon, IDIcon {
@@ -26,7 +27,7 @@ public class ExtMergedIcon implements Icon, IDIcon {
             this.icon = icon;
             this.x = x;
             this.y = y;
-            composite = c;
+            this.composite = c;
             this.z = z;
         }
 
@@ -37,11 +38,11 @@ public class ExtMergedIcon implements Icon, IDIcon {
 
     protected final TreeSet<Entry> entries      = new TreeSet<Entry>(new Comparator<Entry>() {
 
-                                                    @Override
-                                                    public int compare(final Entry o1, final Entry o2) {
-                                                        return new Integer(o1.z).compareTo(new Integer(o2.z));
-                                                    }
-                                                });
+        @Override
+        public int compare(final Entry o1, final Entry o2) {
+            return new Integer(o1.z).compareTo(new Integer(o2.z));
+        }
+    });
 
     private int                    width        = 0;
     private int                    height       = 0;
@@ -57,7 +58,7 @@ public class ExtMergedIcon implements Icon, IDIcon {
     /**
      * @param abstractIcon
      */
-    public ExtMergedIcon(final Icon icon) {
+     public ExtMergedIcon(final Icon icon) {
         this(icon, 0, 0, 0, null);
     }
 
@@ -66,162 +67,163 @@ public class ExtMergedIcon implements Icon, IDIcon {
      * @param i
      * @param j
      */
-    public ExtMergedIcon(final Icon icon, final int x, final int y) {
-        addEntry(new Entry(icon, x, y, 0, null));
-    }
+     public ExtMergedIcon(final Icon icon, final int x, final int y) {
+         this.addEntry(new Entry(icon, x, y, 0, null));
+     }
 
-    public ExtMergedIcon(final Icon icon, final int x, final int y, final int z, final Composite c) {
-        addEntry(new Entry(icon, x, y, z, c));
-    }
+     public ExtMergedIcon(final Icon icon, final int x, final int y, final int z, final Composite c) {
+         this.addEntry(new Entry(icon, x, y, z, c));
+     }
 
-    public ExtMergedIcon add(final Icon icon) {
-        synchronized (entries) {
-            return this.add(icon, 0, 0, entries.size(), null);
-        }
-    }
+     public ExtMergedIcon add(final Icon icon) {
+         synchronized (this.entries) {
+             return this.add(icon, 0, 0, this.entries.size(), null);
+         }
+     }
 
-    /**
-     * @param abstractIcon
-     * @param i
-     * @param j
-     * @return
-     */
-    public ExtMergedIcon add(final Icon icon, final int x, final int y) {
-        synchronized (entries) {
-            return this.add(icon, x, y, entries.size(), null);
-        }
+     /**
+      * @param abstractIcon
+      * @param i
+      * @param j
+      * @return
+      */
+     public ExtMergedIcon add(final Icon icon, final int x, final int y) {
+         synchronized (this.entries) {
+             return this.add(icon, x, y, this.entries.size(), null);
+         }
 
-    }
+     }
 
-    public ExtMergedIcon add(final Icon icon, final int x, final int y, final int z, final Composite c) {
-        addEntry(new Entry(icon, x, y, z, c));
-        return this;
-    }
+     public ExtMergedIcon add(final Icon icon, final int x, final int y, final int z, final Composite c) {
+         this.addEntry(new Entry(icon, x, y, z, c));
+         return this;
+     }
 
-    private void addEntry(final Entry entry) {
-        if (internalIcon != null) { throw new IllegalStateException("internalIcon is set"); }
-        width = Math.max(width, entry.x + entry.icon.getIconWidth());
-        height = Math.max(height, entry.y + entry.icon.getIconHeight());
-        if (!(entry.icon instanceof IDIcon)) {
-            System.out.println(getClass() + ": Warning. Not an  IDIcon");
-            new Exception().printStackTrace();
-        }
-        entries.add(entry);
+     private static final boolean JARED = Application.isJared(ExtMergedIcon.class);
 
-    }
+     private void addEntry(final Entry entry) {
+         if (this.internalIcon != null) { throw new IllegalStateException("internalIcon is set"); }
+         this.width = Math.max(this.width, entry.x + entry.icon.getIconWidth());
+         this.height = Math.max(this.height, entry.y + entry.icon.getIconHeight());
+         if (!ExtMergedIcon.JARED && !(entry.icon instanceof IDIcon)) {
+            new Exception(this.getClass() + ": Warning. Not an  IDIcon").printStackTrace();
+         }
+         this.entries.add(entry);
 
-    public void cache() {
-        caching = true;
-        try {
-            internalIcon = ImageProvider.toImageIcon(this);
-            internalID = getIdentifier();
-            entries.clear();
-        } finally {
-            caching = false;
-        }
-    }
+     }
 
-    // /**
-    // * @return
-    // */
-    // public Icon crop() {
-    // // TODO Auto-generated method stub
-    // return null;
-    // }
+     public void cache() {
+         this.caching = true;
+         try {
+             this.internalIcon = ImageProvider.toImageIcon(this);
+             this.internalID = this.getIdentifier();
+             this.entries.clear();
+         } finally {
+             this.caching = false;
+         }
+     }
 
-    public ExtMergedIcon crop(final int width, final int height) {
-        cropedWidth = width;
-        cropedHeight = height;
+     // /**
+     // * @return
+     // */
+     // public Icon crop() {
+     // // TODO Auto-generated method stub
+     // return null;
+     // }
 
-        return this;
-    }
+     public ExtMergedIcon crop(final int width, final int height) {
+         this.cropedWidth = width;
+         this.cropedHeight = height;
 
-    @Override
-    public int getIconHeight() {
-        if (cropedHeight > 0) { return cropedHeight; }
-        return height;
-    }
+         return this;
+     }
 
-    @Override
-    public int getIconWidth() {
-        if (cropedWidth > 0) { return cropedWidth; }
-        return width;
-    }
+     @Override
+     public int getIconHeight() {
+         if (this.cropedHeight > 0) { return this.cropedHeight; }
+         return this.height;
+     }
 
-    @Override
-    public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
+     @Override
+     public int getIconWidth() {
+         if (this.cropedWidth > 0) { return this.cropedWidth; }
+         return this.width;
+     }
 
-        final Graphics2D g2 = (Graphics2D) g;
+     @Override
+     public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
 
-        if (internalIcon == null && !caching) {
-            cache();
+         final Graphics2D g2 = (Graphics2D) g;
 
-        }
+         if (this.internalIcon == null && !this.caching) {
+             this.cache();
 
-        if (internalIcon != null) {
-            g2.drawImage(internalIcon.getImage(), x, y, null);
-            // internalIcon.paintIcon(c, g2, x, y);
-            return;
-        }
-        final Shape oldClip = g2.getClip();
-        // Rectangle rec = new Rectangle( );
-        g2.setClip(x, y, getIconWidth(), getIconHeight());
-        for (final Entry e : entries) {
-            final Composite com = g2.getComposite();
-            try {
-                if (e.composite != null) {
-                    g2.setComposite(e.composite);
-                }
-                e.icon.paintIcon(c, g2, x + e.x, y + e.y);
-            } finally {
-                if (com != null) {
-                    g2.setComposite(com);
-                }
-            }
+         }
 
-        }
-        g2.setClip(oldClip);
-    }
+         if (this.internalIcon != null) {
+             g2.drawImage(this.internalIcon.getImage(), x, y, null);
+             // internalIcon.paintIcon(c, g2, x, y);
+             return;
+         }
+         final Shape oldClip = g2.getClip();
+         // Rectangle rec = new Rectangle( );
+         g2.setClip(x, y, this.getIconWidth(), this.getIconHeight());
+         for (final Entry e : this.entries) {
+             final Composite com = g2.getComposite();
+             try {
+                 if (e.composite != null) {
+                     g2.setComposite(e.composite);
+                 }
+                 e.icon.paintIcon(c, g2, x + e.x, y + e.y);
+             } finally {
+                 if (com != null) {
+                     g2.setComposite(com);
+                 }
+             }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.appwork.swing.components.IdentifierInterface#toIdentifier()
-     */
-    @Override
-    public IconIdentifier getIdentifier() {
+         }
+         g2.setClip(oldClip);
+     }
 
-        if (internalID != null) { return internalID; }
-        IconIdentifier t = new IconIdentifier("Merge");
-        if (cropedHeight > 0) {
-            t.addProperty("cropedHeight", cropedHeight);
-        }
-        if (cropedWidth > 0) {
-            t.addProperty("cropedWidth", cropedWidth);
-        }
-        t.addProperty("height", getIconHeight());
-        t.addProperty("width", getIconWidth());
+     /*
+      * (non-Javadoc)
+      *
+      * @see org.appwork.swing.components.IdentifierInterface#toIdentifier()
+      */
+     @Override
+     public IconIdentifier getIdentifier() {
 
-        for (Entry e : entries) {
-            if (e.icon instanceof IDIcon) {
-                IconIdentifier id = ((IDIcon) e.icon).getIdentifier();
-                if (e.x != 0) {
-                    id.addProperty("x", e.x);
-                }
-                if (e.y != 0) {
-                    id.addProperty("y", e.y);
-                }
-                id.addProperty("height", e.icon.getIconHeight());
-                id.addProperty("width", e.icon.getIconWidth());
-                t.add(id);
-                //
+         if (this.internalID != null) { return this.internalID; }
+         IconIdentifier t = new IconIdentifier("Merge");
+         if (this.cropedHeight > 0) {
+             t.addProperty("cropedHeight", this.cropedHeight);
+         }
+         if (this.cropedWidth > 0) {
+             t.addProperty("cropedWidth", this.cropedWidth);
+         }
+         t.addProperty("height", this.getIconHeight());
+         t.addProperty("width", this.getIconWidth());
 
-            } else {
-                t.add(new IconIdentifier("unknown", e.icon.toString()));
+         for (Entry e : this.entries) {
+             if (e.icon instanceof IDIcon) {
+                 IconIdentifier id = ((IDIcon) e.icon).getIdentifier();
+                 if (e.x != 0) {
+                     id.addProperty("x", e.x);
+                 }
+                 if (e.y != 0) {
+                     id.addProperty("y", e.y);
+                 }
+                 id.addProperty("height", e.icon.getIconHeight());
+                 id.addProperty("width", e.icon.getIconWidth());
+                 t.add(id);
+                 //
 
-            }
-        }
+             } else {
+                 t.add(new IconIdentifier("unknown", e.icon.toString()));
 
-        return t;
-    }
+             }
+         }
+
+         return t;
+     }
 }

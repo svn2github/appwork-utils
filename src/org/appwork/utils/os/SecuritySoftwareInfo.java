4ea1647ecb22d0a3a0489a11fad013647a6b3f4e
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-07-10 15:12:13 +0000

    git-svn-id: svn://svn.appwork.org/utils@2374 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/os/SecuritySoftwareInfo.java
+++ src/org/appwork/utils/os/SecuritySoftwareInfo.java
@@ -21,4 +21,60 @@ public class SecuritySoftwareInfo extends HashMap<String, String> {
 
         return get("displayName");
     }
+
+    public String getState() {
+        return get("productState");
+    }
+
+    /**
+     * http://neophob.com/2010/03/wmi-query-windows-securitycenter2/
+     * 
+     * @return
+     */
+    public boolean isEnabled() {
+
+        String state = getState();
+        if (state == null) {
+            return false;
+        }
+        int i = Integer.parseInt(state);
+        // String hex = StringUtils.fillPre(Integer.toHexString(i), "0", 6);
+        // int upToDate = 0xFF & i;
+        int enabledFlag = 0xFF & i >> 8;
+        // int type = 0xFF & i >> 16;
+        // System.out.println(Integer.toHexString(enabled) + " " + Integer.toHexString(enabledFlag) + " " + Integer.toHexString(c));
+        // if ("AVG AntiVirus Free Edition 2015".equals(getName())) {
+        // System.out.println(getState() + "\t-> " + hex + "\t" + (enabledFlag >= 16) + " " + Regex.getLines(get("response")).length);
+        // }
+        return enabledFlag >= 16;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.util.AbstractMap#toString()
+     */
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return getName() + " Enabled: " + isEnabled() + " Up2Date: " + isUp2Date();
+    }
+
+    public boolean isUp2Date() {
+        String up2DateXp = get("productUptoDate");
+        if ("TRUE".equalsIgnoreCase(up2DateXp)) {
+            return true;
+        }
+        String state = getState();
+        if (state == null) {
+            return false;
+        }
+        int i = Integer.parseInt(state);
+        // String hex = StringUtils.fillPre(Integer.toHexString(i), "0", 6);
+        int upToDate = 0xFF & i;
+        // int enabledFlag = 0xFF & i >> 8;
+        // int type = 0xFF & i >> 16;
+        // System.out.println(Integer.toHexString(enabled) + " " + Integer.toHexString(enabledFlag) + " " + Integer.toHexString(c));
+        return upToDate == 0;
+    }
 }

b853d6483b4549f2094804f3484dffd1bc8e572d
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-09-11 11:01:33 +0000

    build>full
    git-svn-id: svn://svn.appwork.org/utils@2431 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/JarHandlerWorkaroundOracle.java
+++ src/org/appwork/utils/JarHandlerWorkaroundOracle.java
@@ -9,14 +9,10 @@
  */
 package org.appwork.utils;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.jar.JarEntry;
 
 /**
  * @author daniel
@@ -35,78 +31,7 @@ public class JarHandlerWorkaroundOracle {
                     @Override
                     public URLStreamHandler createURLStreamHandler(String protocol) {
                         if ("jar".equals(protocol)) {
-                            return new sun.net.www.protocol.jar.Handler() {
-
-                                @Override
-                                protected URLConnection openConnection(final URL url) throws IOException {
-                                    try {
-                                        System.out.println("openConnection:" + url);
-                                        final String path = url.getFile();
-                                        URL jarFileURL = null;
-                                        int lastIndex = 0;
-                                        if (StringUtils.startsWithCaseInsensitive(path, "file:")) {
-                                            while (true) {
-                                                final int indexOf = path.indexOf(".jar", lastIndex);
-                                                if (indexOf > 0) {
-                                                    final int index = indexOf + 4;
-                                                    lastIndex = index;
-                                                    final String jarFileName = path.substring(0, index);
-                                                    if (jarFileName.contains("!/")) {
-                                                        final File jarFile;
-                                                        final URL jarURL;
-                                                        try {
-                                                            jarURL = new URL(jarFileName);
-                                                            jarFile = new File(jarURL.toURI());
-                                                        } catch (Exception e) {
-                                                            e.printStackTrace();
-                                                            continue;
-                                                        }
-                                                        if (jarFile.exists() && jarFile.isFile()) {
-                                                            jarFileURL = jarURL;
-                                                            break;
-                                                        }
-                                                    }
-                                                } else {
-                                                    break;
-                                                }
-                                            }
-                                        }
-                                        if (jarFileURL != null) {
-                                            System.out.println("Workaround");
-                                            lastIndex++;// skip !
-                                            final URL finalJarFileURL = jarFileURL;
-                                            final String entryName;
-                                            if (++lastIndex != path.length()) {
-                                                final String tempEntryName = path.substring(lastIndex, path.length());
-                                                entryName = sun.net.www.ParseUtil.decode(tempEntryName);
-                                            } else {
-                                                entryName = null;
-                                            }
-                                            System.out.println("Workaround for openConnection:" + finalJarFileURL + " Entry:" + entryName);
-                                            return new sun.net.www.protocol.jar.JarURLConnection(url, this) {
-                                                @Override
-                                                public URL getJarFileURL() {
-                                                    return finalJarFileURL;
-                                                }
-
-                                                @Override
-                                                public JarEntry getJarEntry() throws IOException {
-                                                    return getJarFile().getJarEntry(getEntryName());
-                                                }
-
-                                                @Override
-                                                public String getEntryName() {
-                                                    return entryName;
-                                                }
-                                            };
-                                        }
-                                    } catch (final Throwable e) {
-                                        e.printStackTrace();
-                                    }
-                                    return super.openConnection(url);
-
-                                }
-                            };
+                            return new OracleWorkaroundJarHandler();
                         }
                         return null;
                     }

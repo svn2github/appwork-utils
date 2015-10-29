a2f62663bdff2b95d9780dbcaf053bd9a1148f3f
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-10-29 16:47:21 +0000

    git-svn-id: svn://svn.appwork.org/utils@2487 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/Application.java
+++ src/org/appwork/utils/Application.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  * 	    "MyJDownloader Client" License
  * 	    The "MyJDownloader Client" will be called [The Product] from now on.
@@ -7,27 +7,27 @@
  * 	    Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  * 	    Schwabacher Straße 117
  * 	    90763 Fürth
- * 	    Germany   
+ * 	    Germany
  * === Preamble ===
  * 	This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  * 	The intent is that the AppWork GmbH is able to provide  their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  * 	These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  * 	Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- * 	to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ * 	to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
- * 	If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's as much as a 
+ * 	If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's as much as a
  * 	sniff of commercial interest or aspect in what you are doing, we consider this as a commercial usage. If you are unsure whether your use-case is commercial or not, consider it as commercial.
  * === Dual Licensing ===
  * === Commercial Usage ===
  * 	If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  * 	Contact AppWork for further details: e-mail@appwork.org
  * === Non-Commercial Usage ===
- * 	If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ * 	If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  * 	"GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  * 	If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -606,7 +606,7 @@ public class Application {
             org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().warning("Java 1.6 Update 18 has a serious bug in garbage collector!");
             /*
              * java 1.6 update 18 has a bug in garbage collector, causes java crashes
-             *
+             * 
              * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6847956
              */
             return true;
@@ -629,7 +629,7 @@ public class Application {
             org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().warning("freezing AppKit thread bug");
             /*
              * http://bugs.java.com/view_bug.do?bug_id=8025588
-             *
+             * 
              * Frozen AppKit thread
              */
             return true;
@@ -697,7 +697,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         *
+         * 
          * @see java.io.OutputStream#write(int)
          */
         @Override
@@ -721,7 +721,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         *
+         * 
          * @see java.io.OutputStream#write(byte[])
          */
         @Override
@@ -744,7 +744,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         *
+         * 
          * @see java.io.OutputStream#write(byte[], int, int)
          */
         @Override
@@ -768,7 +768,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         *
+         * 
          * @see java.io.OutputStream#flush()
          */
         @Override
@@ -791,7 +791,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         *
+         * 
          * @see java.io.OutputStream#close()
          */
         @Override
@@ -900,10 +900,9 @@ public class Application {
      */
     public synchronized static void setApplication(final String newAppFolder) {
         warnInit();
-        Application.ensureFrameWorkInit();
-
         Application.ROOT = null;
         Application.APP_FOLDER = newAppFolder;
+        Application.ensureFrameWorkInit();
     }
 
     /**

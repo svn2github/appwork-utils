7376a3d3c409d977817d6bbd3e1b4f1298c8e9e0
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-11-11 14:43:16 +0000

    fixes #66271 optional: stop timeout only on click on timer!
    Stop countdown on mouse move is now disabled by default.
    Stop countdown on mouse move is not triggered any more if the dialog pops up at the current mouse position
    
    git-svn-id: svn://svn.appwork.org/utils@2500 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/ExtTextField.java
+++ src/org/appwork/swing/components/ExtTextField.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -53,7 +53,7 @@ import org.appwork.utils.swing.SwingUtils;
 
 public class ExtTextField extends JTextField implements CaretListener, FocusListener, DocumentListener, ContextMenuAdapter, TextComponentInterface {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID     = -3625278218179478516L;
     protected Color           defaultColor;
@@ -81,9 +81,8 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
 
     /*
      * (non-Javadoc)
-     * 
-     * @see javax.swing.event.DocumentListener#changedUpdate(javax.swing.event.
-     * DocumentEvent)
+     *
+     * @see javax.swing.event.DocumentListener#changedUpdate(javax.swing.event. DocumentEvent)
      */
     @Override
     public void changedUpdate(final DocumentEvent e) {
@@ -97,7 +96,9 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
     }
 
     public void focusGained(final FocusEvent arg0) {
-        if (!this.isHelperEnabled()) { return; }
+        if (!this.isHelperEnabled()) {
+            return;
+        }
         if (super.getText().equals(this.helpText)) {
             if (this.isClearHelpTextOnFocus()) {
                 this.setText("");
@@ -111,7 +112,9 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
     }
 
     public void focusLost(final FocusEvent arg0) {
-        if (!this.isHelperEnabled()) { return; }
+        if (!this.isHelperEnabled()) {
+            return;
+        }
         if (this.getDocument().getLength() == 0 || super.getText().equals(this.helpText)) {
             this.setText(this.helpText);
             this.setForeground(this.helpColor);
@@ -137,14 +140,10 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
 
     /*
      * (non-Javadoc)
-     * 
-     * @see
-     * org.appwork.app.gui.copycutpaste.ContextMenuAdapter#getPopupMenu(org.
-     * appwork.app.gui.copycutpaste.CutAction,
-     * org.appwork.app.gui.copycutpaste.CopyAction,
-     * org.appwork.app.gui.copycutpaste.PasteAction,
-     * org.appwork.app.gui.copycutpaste.DeleteAction,
-     * org.appwork.app.gui.copycutpaste.SelectAction)
+     *
+     * @see org.appwork.app.gui.copycutpaste.ContextMenuAdapter#getPopupMenu(org. appwork.app.gui.copycutpaste.CutAction,
+     * org.appwork.app.gui.copycutpaste.CopyAction, org.appwork.app.gui.copycutpaste.PasteAction,
+     * org.appwork.app.gui.copycutpaste.DeleteAction, org.appwork.app.gui.copycutpaste.SelectAction)
      */
     @Override
     public JPopupMenu getPopupMenu(final AbstractAction cutAction, final AbstractAction copyAction, final AbstractAction pasteAction, final AbstractAction deleteAction, final AbstractAction selectAction) {
@@ -170,7 +169,9 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
     @Override
     public String getText() {
         String ret = super.getText();
-        if (!this.isHelperEnabled()) { return ret; }
+        if (!this.isHelperEnabled()) {
+            return ret;
+        }
         if (ret.equals(this.helpText) && this.getForeground() == this.helpColor) {
             ret = "";
         }
@@ -179,9 +180,8 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
 
     /*
      * (non-Javadoc)
-     * 
-     * @see javax.swing.event.DocumentListener#insertUpdate(javax.swing.event.
-     * DocumentEvent)
+     *
+     * @see javax.swing.event.DocumentListener#insertUpdate(javax.swing.event. DocumentEvent)
      */
     @Override
     public void insertUpdate(final DocumentEvent e) {
@@ -203,7 +203,7 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
     }
 
     /**
-     * 
+     *
      */
     public void onChanged() {
         // TODO Auto-generated method stub
@@ -211,9 +211,8 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
 
     /*
      * (non-Javadoc)
-     * 
-     * @see javax.swing.event.DocumentListener#removeUpdate(javax.swing.event.
-     * DocumentEvent)
+     *
+     * @see javax.swing.event.DocumentListener#removeUpdate(javax.swing.event. DocumentEvent)
      */
     @Override
     public void removeUpdate(final DocumentEvent e) {
@@ -263,6 +262,11 @@ public class ExtTextField extends JTextField implements CaretListener, FocusList
 
     @Override
     public void setText(String t) {
+        String isText = getText();
+
+        if (StringUtils.equals(t == null ? "" : t, isText)) {
+            return;
+        }
         if (!this.isHelperEnabled()) {
             super.setText(t);
             return;

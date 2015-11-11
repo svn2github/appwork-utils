bb16ba2e8d4c3405a6cad5c18632a01e89ba24aa
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-11-11 15:37:02 +0000

    SearchComboBox: -fixed thread 354712
    git-svn-id: svn://svn.appwork.org/utils@2501 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/searchcombo/SearchComboBox.java
+++ src/org/appwork/swing/components/searchcombo/SearchComboBox.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -84,7 +84,6 @@ import org.appwork.app.gui.BasicGui;
 import org.appwork.resources.AWUTheme;
 import org.appwork.swing.MigPanel;
 import org.appwork.utils.StringUtils;
-
 import org.appwork.utils.swing.EDTRunner;
 import org.appwork.utils.swing.SwingUtils;
 
@@ -554,6 +553,10 @@ public abstract class SearchComboBox<T> extends JComboBox {
 
     public SearchComboBox(final List<T> plugins) {
         super((ComboBoxModel) null);
+        this.setEditor(new Editor(this));
+        if (plugins != null) {
+            this.setList(plugins);
+        }
         this.addFocusListener(new FocusListener() {
 
             @Override
@@ -568,10 +571,6 @@ public abstract class SearchComboBox<T> extends JComboBox {
 
             }
         });
-        if (plugins != null) {
-            this.setList(plugins);
-        }
-        this.setEditor(new Editor(this));
         this.setEditable(true);
         // we extends the existing renderer. this avoids LAF incompatibilities
         final ListCellRenderer org = this.getRenderer();
@@ -698,7 +697,7 @@ public abstract class SearchComboBox<T> extends JComboBox {
 
     @SuppressWarnings("unchecked")
     public JTextField getTextField() {
-        if ((Editor) this.getEditor() == null) {
+        if (this.getEditor() == null) {
             return null;
         }
         return ((Editor) this.getEditor()).getTf();
@@ -712,7 +711,15 @@ public abstract class SearchComboBox<T> extends JComboBox {
 
     @Override
     public boolean hasFocus() {
-        return super.hasFocus() || this.getTextField().hasFocus();
+        if (super.hasFocus()) {
+            return true;
+        } else {
+            final ComboBoxEditor lEditor = getEditor();
+            if (lEditor instanceof SearchComboBox.Editor) {
+                return ((SearchComboBox.Editor) lEditor).getTf().hasFocus();
+            }
+        }
+        return false;
     }
 
     public boolean isAutoCompletionEnabled() {

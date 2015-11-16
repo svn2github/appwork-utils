c9cc9a869621e9bdb258ee2c60ec4cd3b69c28a4
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-11-16 11:51:43 +0000

    Added TimeTracker class to  defined limits for certain events. Used to limit captcha requests and avoid ddos like behaviour Added  limits for recaptcha 1 and 2 Limits for captcha serices should be added in the ChallengeResponseController class refs #65700 ____________________________________________________ Bug:GUI: JD2 is not restoring from tray to a maximized window anymore fixes #75461
    This fix is tested under windows 10. requires further testing. 
    ____________________________________________________
    
    Fixes this bug:
    When JD exits in maximized state, it will restore the size and state on the next restart. IF JD is normalized afterwards, JD did not restore the correct last actual dimension.
    
    This fix is tested under windows 10. requires further testing. 
    ____________________________________________________
    
    
    
    git-svn-id: svn://svn.appwork.org/utils@2503 21714237-3853-44ef-a1f0-ef8f03a7d1fe

---
+++ src/org/appwork/timetracker/TimeTracker.java
@@ -0,0 +1,163 @@
+/**
+ *
+ * ====================================================================================================================================================
+ *         "AppWork Utilities" License
+ *         The "AppWork Utilities" will be called [The Product] from now on.
+ * ====================================================================================================================================================
+ *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
+ *         Schwabacher Straße 117
+ *         90763 Fürth
+ *         Germany
+ * === Preamble ===
+ *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
+ *     The intent is that the AppWork GmbH is able to provide  their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
+ *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
+ *
+ * === 3rd Party Licences ===
+ *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
+ * === Definition: Commercial Usage ===
+ *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
+ *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact as.
+ * === Dual Licensing ===
+ * === Commercial Usage ===
+ *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
+ *     Contact AppWork for further details: e-mail@appwork.org
+ * === Non-Commercial Usage ===
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
+ *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
+ *
+ *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
+ * ====================================================================================================================================================
+ * ==================================================================================================================================================== */
+package org.appwork.timetracker;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import org.appwork.utils.formatter.TimeFormatter;
+import org.appwork.utils.logging2.LogSource;
+import org.appwork.utils.logging2.extmanager.LoggerFactory;
+
+/**
+ * @author thomas
+ * @date 12.11.2015
+ *
+ */
+public class TimeTracker {
+    private String                   id;
+    private ArrayList<TrackerRule>   rules;
+    private LinkedList<TrackedEntry> entries;
+    private LogSource                logger;
+
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param typeID
+     */
+    public TimeTracker(String typeID) {
+        this.id = typeID;
+        this.rules = new ArrayList<TrackerRule>();
+        entries = new LinkedList<TrackedEntry>();
+        logger = LoggerFactory.I().getLogger("TimeTracker " + id);
+
+    }
+
+    public void addRule(TrackerRule rule) {
+        ArrayList<TrackerRule> newRules = new ArrayList<TrackerRule>(rules);
+
+        newRules.add(rule);
+        rules = newRules;
+
+    }
+
+    public void wait(TrackerJob job) throws InterruptedException {
+        if (rules.size() == 0) {
+            return;
+        }
+        long waitFor = 0;
+
+        synchronized (this) {
+
+            waitFor = getWaitFor(job.getWeight());
+
+            entries.add(createEntry(waitFor, job.getWeight()));
+
+        }
+
+        if (waitFor > 0) {
+            logger.info("Wait " + TimeFormatter.formatMilliSeconds(waitFor, 0));
+            job.waitForNextSlot(waitFor);
+        }
+
+        job.run();
+        logger.info("RUN");
+
+    }
+
+    /**
+     * @param weight
+     * @return
+     */
+    public long getWaitFor(int weight) {
+
+        int sum = 0;
+        LinkedList<TrackerRule> lRules = new LinkedList<TrackerRule>(rules);
+        ListIterator<TrackedEntry> itEntres = entries.listIterator(entries.size());
+        long time = System.currentTimeMillis();
+        logger.info("Find wait For");
+        long interval = 0;
+        while (itEntres.hasPrevious()) {
+
+            TrackedEntry entry = itEntres.previous();
+            logger.info("Check Entry " + new Date(entry.getTime()));
+            // time = Math.max(entry.getTime(), time);
+            sum += entry.getWeight();
+
+            Iterator<TrackerRule> it = lRules.iterator();
+            while (it.hasNext()) {
+                TrackerRule r = it.next();
+
+                long diff = time - entry.getTime();
+                if (diff > r.getInterval()) {
+                    it.remove();
+
+                    if (lRules.size() == 0) {
+                        logger.info("" + sum + "/" + r.getAmount() + " in the last " + r.getInterval());
+                        // cleanup
+                        while (itEntres.hasPrevious()) {
+                            itEntres.previous();
+                            itEntres.remove();
+                        }
+                        return interval;
+                    }
+                    continue;
+                } else if (sum >= r.getAmount()) {
+                    long ninterval = Math.max(interval, r.getInterval() - diff);
+                    if (ninterval > interval) {
+                        logger.info("" + sum + "/" + r.getAmount() + " in the last " + r.getInterval() + " -> Wait " + TimeFormatter.formatMilliSeconds(interval, 0));
+                        interval = ninterval;
+                    }
+                }
+            }
+
+        }
+        logger.info("" + sum + " --> " + TimeFormatter.formatMilliSeconds(interval, 0));
+        return interval;
+    }
+
+    /**
+     * @param waitFor
+     * @param weight
+     * @return
+     */
+    protected TrackedEntry createEntry(long waitFor, int weight) {
+        return new TrackedEntry(weight, waitFor);
+    }
+}

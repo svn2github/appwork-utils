51fdf1786b4742fea0ecc22675973ff1b76e3f73
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-12-04 08:53:51 +0000

    build>full Debug code: Find Uninstaller Bug
    git-svn-id: svn://svn.appwork.org/utils@2518 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/storage/config/JsonConfig.java
+++ src/org/appwork/storage/config/JsonConfig.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -39,24 +39,27 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.swing.JOptionPane;
+
 import org.appwork.exceptions.WTFException;
 import org.appwork.storage.config.annotations.CustomStorageName;
 import org.appwork.storage.config.handler.StorageHandler;
 import org.appwork.utils.Application;
+import org.appwork.utils.Exceptions;
 import org.appwork.utils.swing.dialog.Dialog;
 
 /**
  * @author thomas
- * 
+ *
  */
 public class JsonConfig {
 
     private static class LockObject {
-        private final String        id;
+        private final String id;
 
-        private final AtomicInteger lock           = new AtomicInteger(0);
+        private final AtomicInteger lock = new AtomicInteger(0);
 
-        private StorageHandler      storageHandler = null;
+        private StorageHandler storageHandler = null;
 
         private LockObject(final String id) {
             this.id = id;
@@ -82,7 +85,7 @@ public class JsonConfig {
 
     private static final HashMap<String, ConfigInterface> CACHE = new HashMap<String, ConfigInterface>();
 
-    private static final HashMap<String, LockObject>      LOCKS = new HashMap<String, LockObject>();
+    private static final HashMap<String, LockObject> LOCKS = new HashMap<String, LockObject>();
 
     /**
      * @param <T>
@@ -98,14 +101,18 @@ public class JsonConfig {
         }
         synchronized (JsonConfig.CACHE) {
             final ConfigInterface ret = JsonConfig.CACHE.get(path);
-            if (ret != null) { return (T) ret; }
+            if (ret != null) {
+                return (T) ret;
+            }
         }
         final LockObject lock = JsonConfig.requestLock(path);
         synchronized (lock) {
             try {
                 synchronized (JsonConfig.CACHE) {
                     final ConfigInterface ret = JsonConfig.CACHE.get(path);
-                    if (ret != null) { return (T) ret; }
+                    if (ret != null) {
+                        return (T) ret;
+                    }
                 }
                 final ClassLoader cl = configInterface.getClassLoader();
                 if (lock.getStorageHandler() == null) {
@@ -120,6 +127,7 @@ public class JsonConfig {
                 return ret;
             } catch (final RuntimeException e) {
                 e.printStackTrace();
+                JOptionPane.showMessageDialog(null, Exceptions.getStackTrace(e), "Error!", 0, null);
                 Dialog.getInstance().showExceptionDialog(e.getClass().getSimpleName(), e.getMessage(), e);
                 throw e;
             } finally {
@@ -137,14 +145,18 @@ public class JsonConfig {
         final String id = path.getAbsolutePath() + configInterface.getName();
         synchronized (JsonConfig.CACHE) {
             final ConfigInterface ret = JsonConfig.CACHE.get(id);
-            if (ret != null) { return (T) ret; }
+            if (ret != null) {
+                return (T) ret;
+            }
         }
         final LockObject lock = JsonConfig.requestLock(id);
         synchronized (lock) {
             try {
                 synchronized (JsonConfig.CACHE) {
                     final ConfigInterface ret = JsonConfig.CACHE.get(id);
-                    if (ret != null) { return (T) ret; }
+                    if (ret != null) {
+                        return (T) ret;
+                    }
                 }
                 final ClassLoader cl = configInterface.getClassLoader();
                 if (lock.getStorageHandler() == null) {
@@ -171,14 +183,18 @@ public class JsonConfig {
         final String id = urlPath + configInterface.getName();
         synchronized (JsonConfig.CACHE) {
             final ConfigInterface ret = JsonConfig.CACHE.get(id);
-            if (ret != null) { return (T) ret; }
+            if (ret != null) {
+                return (T) ret;
+            }
         }
         final LockObject lock = JsonConfig.requestLock(id);
         synchronized (lock) {
             try {
                 synchronized (JsonConfig.CACHE) {
                     final ConfigInterface ret = JsonConfig.CACHE.get(id);
-                    if (ret != null) { return (T) ret; }
+                    if (ret != null) {
+                        return (T) ret;
+                    }
                 }
                 final ClassLoader cl = configInterface.getClassLoader();
                 if (lock.getStorageHandler() == null) {

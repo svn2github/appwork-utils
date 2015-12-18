fd2dc188898d8e2987082d9f159c53560f8ceb1f
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2015-12-18 12:09:31 +0000

    git-svn-id: svn://svn.appwork.org/utils@2528 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/swing/dialog/LoginDialog.java
+++ src/org/appwork/utils/swing/dialog/LoginDialog.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -46,13 +46,13 @@ import javax.swing.JTextField;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 
-import net.miginfocom.swing.MigLayout;
-
 import org.appwork.resources.AWUTheme;
 import org.appwork.utils.BinaryLogic;
 import org.appwork.utils.locale._AWU;
 import org.appwork.utils.swing.EDTHelper;
 import org.appwork.utils.swing.dialog.LoginDialog.LoginData;
+
+import net.miginfocom.swing.MigLayout;
 
 /**
  * @author thomas
@@ -174,12 +174,16 @@ public class LoginDialog extends AbstractDialog<LoginData> implements ActionList
         contentpane.add(accid, "sizegroup g1,width 100:250:n");
         contentpane.add(addSettingName(_AWU.T.AccountNew_layoutDialogContent_password()));
         contentpane.add(pass, "sizegroup g1");
-        contentpane.add(addSettingName(_AWU.T.AccountNew_layoutDialogContent_save()));
-        contentpane.add(save, "sizegroup g1");
+        addSave(contentpane);
         accid.setText(preUser);
         pass.setText(prePass);
         save.setSelected(preSave);
         return contentpane;
+    }
+
+    protected void addSave(final JPanel contentpane) {
+        contentpane.add(addSettingName(_AWU.T.AccountNew_layoutDialogContent_save()));
+        contentpane.add(save, "sizegroup g1");
     }
 
     @Override

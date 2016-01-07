67f02c87d9721a9888200117aebe7f62b6217b08
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-01-07 13:54:41 +0000

    git-svn-id: svn://svn.appwork.org/utils@2534 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/remoteapi/InterfaceHandler.java
+++ src/org/appwork/remoteapi/InterfaceHandler.java
@@ -297,7 +297,7 @@ public class InterfaceHandler<T> {
                     if (Application.getJavaVersion() >= Application.JAVA18) {
                         final java.lang.reflect.Parameter param = m.getParameters()[i];
                         if (param.isNamePresent()) {
-                            paramName = param.getName();
+                            paramName = paramName + "-" + param.getName();
                         }
                     }
                 } catch (final Throwable e) {

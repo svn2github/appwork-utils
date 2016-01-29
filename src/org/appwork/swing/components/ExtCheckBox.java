0adb39247c71e59f892c5ded77ba112b10e283ef
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-01-29 13:25:03 +0000

    git-svn-id: svn://svn.appwork.org/utils@2555 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/ExtCheckBox.java
+++ src/org/appwork/swing/components/ExtCheckBox.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -50,10 +50,10 @@ import org.appwork.swing.components.tooltips.TooltipTextDelegateFactory;
 import org.appwork.utils.swing.EDTRunner;
 import org.appwork.utils.swing.SwingUtils;
 
-public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
+public class ExtCheckBox extends JCheckBox implements ToolTipHandler {
 
     /**
-     * 
+     *
      */
     private static final long          serialVersionUID = 3223817461429862778L;
     private JComponent[]               dependencies;
@@ -68,7 +68,7 @@ public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
         super();
         tooltipFactory = new TooltipTextDelegateFactory(this);
         SwingUtils.setOpaque(this, false);
-//        addActionListener(this);
+        // addActionListener(this);
         setDependencies(components);
 
     }
@@ -86,26 +86,27 @@ public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
         updateDependencies();
 
     }
+
     public void setModel(final ButtonModel newModel) {
         super.setModel(newModel);
         newModel.addItemListener(new ItemListener() {
-            
+
             @Override
             public void itemStateChanged(final ItemEvent e) {
-              
-                new EDTRunner(){
 
-                 @Override
-                 protected void runInEDT() {
-                    updateDependencies();
-                 }
-                    
-                }; 
+                new EDTRunner() {
+
+                    @Override
+                    protected void runInEDT() {
+                        updateDependencies();
+                    }
+
+                };
             }
         });
-       
-      
+
     }
+
     public JComponent[] getDependencies() {
         return dependencies;
     }
@@ -124,41 +125,41 @@ public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
         this.tooltipFactory = tooltipFactory;
     }
 
-//    public void setSelected(boolean b) {
-//
-//        super.setSelected(b);
-//
-//        updateDependencies();
-//    }
+    // public void setSelected(boolean b) {
+    //
+    // super.setSelected(b);
+    //
+    // updateDependencies();
+    // }
 
-//    /*
-//     * (non-Javadoc)
-//     * 
-//     * @see
-//     * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-//     */
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        updateDependencies();
-//    }
+    // /*
+    // * (non-Javadoc)
+    // *
+    // * @see
+    // * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+    // */
+    // @Override
+    // public void actionPerformed(ActionEvent e) {
+    // updateDependencies();
+    // }
 
     /**
-     * 
+     *
      */
     public void updateDependencies() {
-
         if (dependencies != null) {
             for (final JComponent c : dependencies) {
-                c.setEnabled(getDependenciesLogic(c,isSelected()));
+                c.setEnabled(getDependenciesLogic(c, isSelected()));
             }
         }
 
     }
+
     /**
- * @param c
-     * @param b 
- * @return
- */
+     * @param c
+     * @param b
+     * @return
+     */
     protected boolean getDependenciesLogic(final JComponent c, final boolean b) {
         // TODO Auto-generated method stub
         return b;
@@ -173,9 +174,7 @@ public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * org.appwork.swing.components.tooltips.ToolTipHandler#createExtTooltip
-     * (java.awt.Point)
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler#createExtTooltip (java.awt.Point)
      */
     @Override
     public ExtTooltip createExtTooltip(final Point mousePosition) {
@@ -186,8 +185,7 @@ public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
     /*
      * (non-Javadoc)
      * 
-     * @see org.appwork.swing.components.tooltips.ToolTipHandler#
-     * isTooltipDisabledUntilNextRefocus()
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler# isTooltipDisabledUntilNextRefocus()
      */
     @Override
     public boolean isTooltipDisabledUntilNextRefocus() {
@@ -198,12 +196,16 @@ public class ExtCheckBox extends JCheckBox implements  ToolTipHandler {
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * org.appwork.swing.components.tooltips.ToolTipHandler#updateTooltip(org
-     * .appwork.swing.components.tooltips.ExtTooltip, java.awt.event.MouseEvent)
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler#updateTooltip(org .appwork.swing.components.tooltips.ExtTooltip,
+     * java.awt.event.MouseEvent)
      */
     @Override
-    public int getTooltipDelay(final Point mousePositionOnScreen) {  return 0;    }  @Override public boolean updateTooltip(final ExtTooltip activeToolTip, final MouseEvent e) {
+    public int getTooltipDelay(final Point mousePositionOnScreen) {
+        return 0;
+    }
+
+    @Override
+    public boolean updateTooltip(final ExtTooltip activeToolTip, final MouseEvent e) {
         // TODO Auto-generated method stub
         return false;
     }

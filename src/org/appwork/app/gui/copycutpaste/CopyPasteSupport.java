30ad8a00a87c78b881effd0903ff8d6419670fc4
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-02-12 11:53:07 +0000

    Added SVG Salamander Library for basic SVG support
    git-svn-id: svn://svn.appwork.org/utils@2579 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/app/gui/copycutpaste/CopyPasteSupport.java
+++ src/org/appwork/app/gui/copycutpaste/CopyPasteSupport.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -49,7 +49,7 @@ import javax.swing.text.JTextComponent;
 
 /**
  * @author thomas
- * 
+ *
  */
 public class CopyPasteSupport implements AWTEventListener {
     private long startupTime = System.currentTimeMillis();
@@ -66,19 +66,25 @@ public class CopyPasteSupport implements AWTEventListener {
 
     /*
      * (non-Javadoc)
-     * 
-     * @see
-     * org.appwork.swing.event.EDTBasicListener#onAWTEventAfterDispatch(java
-     * .awt.AWTEvent)
+     *
+     * @see org.appwork.swing.event.EDTBasicListener#onAWTEventAfterDispatch(java .awt.AWTEvent)
      */
     @Override
     public void eventDispatched(final AWTEvent event) {
-        if (!(event instanceof MouseEvent)) { return; }
+        if (!(event instanceof MouseEvent)) {
+            return;
+        }
+
         lastMouseEvent = System.currentTimeMillis();
         final MouseEvent mouseEvent = (MouseEvent) event;
 
-        if (!mouseEvent.isPopupTrigger() && mouseEvent.getButton() != MouseEvent.BUTTON3) { return; }
-        if (mouseEvent.getComponent() == null) { return; }
+        if (!mouseEvent.isPopupTrigger() && mouseEvent.getButton() != MouseEvent.BUTTON3) {
+            return;
+        }
+        if (mouseEvent.getComponent() == null) {
+            return;
+        }
+        System.out.println(event);
         // get deepest component
         // Component c = null;
         final Point point = mouseEvent.getPoint();
@@ -97,22 +103,28 @@ public class CopyPasteSupport implements AWTEventListener {
 
             return;
         }
-
+        System.out.println("Point " + c);
         // Check if deepest component is a textcomponent
-        if (!(c instanceof JTextComponent) && !(c instanceof ContextMenuAdapter)) { return; }
-        if (MenuSelectionManager.defaultManager().getSelectedPath().length > 0) { return; }
- 
+        if (!(c instanceof JTextComponent) && !(c instanceof ContextMenuAdapter)) {
+            return;
+        }
+        if (MenuSelectionManager.defaultManager().getSelectedPath().length > 0) {
+            return;
+        }
+
         JPopupMenu menu;
         final JTextComponent t = (JTextComponent) c;
         t.requestFocus();
         if (c instanceof ContextMenuAdapter) {
             menu = ((ContextMenuAdapter) c).getPopupMenu(createCutAction(t), createCopyAction(t), createPasteAction(t), createDeleteAction(t), createSelectAction(t));
-            if (menu == null) { return; }
+            if (menu == null) {
+                return;
+            }
         } else {
 
             // create menu
             menu = new JPopupMenu();
-      
+
             menu.add(createCutAction(t));
             menu.add(createCopyAction(t));
             menu.add(createPasteAction(t));

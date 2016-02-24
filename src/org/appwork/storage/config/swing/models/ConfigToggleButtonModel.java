4f5f6f523987f2da0b3ce4a77c2cef5378ea62b0
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-02-24 13:05:26 +0000

    Fixed Toggle Button toggle bug when the config backend listener executed EDT code
    git-svn-id: svn://svn.appwork.org/utils@2604 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/storage/config/swing/models/ConfigToggleButtonModel.java
+++ src/org/appwork/storage/config/swing/models/ConfigToggleButtonModel.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -36,6 +36,7 @@ package org.appwork.storage.config.swing.models;
 import java.awt.event.ItemEvent;
 
 import javax.swing.JToggleButton.ToggleButtonModel;
+import javax.swing.SwingUtilities;
 
 import org.appwork.storage.config.ValidationException;
 import org.appwork.storage.config.events.GenericConfigEventListener;
@@ -55,6 +56,7 @@ public class ConfigToggleButtonModel extends ToggleButtonModel implements Generi
 
         this.keyHandler = keyHandler;
         keyHandler.getEventSender().addListener(this, true);
+        setSelected(keyHandler.isEnabled());
 
     }
 
@@ -63,11 +65,21 @@ public class ConfigToggleButtonModel extends ToggleButtonModel implements Generi
     }
 
     public boolean isSelected() {
+
         return keyHandler.isEnabled();
     }
 
     public void setSelected(final boolean b) {
-        keyHandler.setValue(b);
+
+        // else this would hook into the button focus logic an might result in additional unwanted toggle calls
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                keyHandler.setValue(b);
+
+            }
+        });
 
     }
 
@@ -80,8 +92,10 @@ public class ConfigToggleButtonModel extends ToggleButtonModel implements Generi
 
             @Override
             protected void runInEDT() {
+
                 fireStateChanged();
                 fireItemStateChanged();
+
             }
         };
 
@@ -92,8 +106,9 @@ public class ConfigToggleButtonModel extends ToggleButtonModel implements Generi
 
             @Override
             protected void runInEDT() {
-                fireStateChanged();
-                fireItemStateChanged();
+
+                setSelected(ConfigToggleButtonModel.this.keyHandler.isEnabled());
+
             }
         };
 

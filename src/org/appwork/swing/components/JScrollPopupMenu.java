41aca0ed0d937bce1c5c07eac3fc00bdcf5e4224
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-04-05 15:28:26 +0000

    Youtube Refactoring. Added lot's of itags. Added Itag->YoutubeVariant Script ITagHelper
    git-svn-id: svn://svn.appwork.org/utils@2641 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/JScrollPopupMenu.java
+++ src/org/appwork/swing/components/JScrollPopupMenu.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -45,6 +45,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.io.IOException;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
@@ -52,20 +53,22 @@ import javax.swing.JScrollBar;
 
 import org.appwork.app.gui.BasicGui;
 import org.appwork.swing.MigPanel;
+import org.appwork.swing.synthetica.SyntheticaHelper;
+import org.appwork.utils.Application;
 
 /**
- * Source:
- * http://stackoverflow.com/questions/9288350/adding-vertical-scroll-to-a
- * -jpopupmenu
- * 
+ * Source: http://stackoverflow.com/questions/9288350/adding-vertical-scroll-to-a -jpopupmenu
+ *
  * @author thomas
- * 
+ *
  */
 
 public class JScrollPopupMenu extends JPopupMenu {
     protected int maximumVisibleRows = 30;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Application.setApplication(".appwork");
+        SyntheticaHelper.init();
         new BasicGui(JScrollPopupMenu.class.getName()) {
 
             @Override
@@ -97,6 +100,16 @@ public class JScrollPopupMenu extends JPopupMenu {
                     @Override
                     public void mouseClicked(MouseEvent e) {
                         JScrollPopupMenu p = new JScrollPopupMenu();
+                        JScrollMenu sm = new JScrollMenu("Video");
+                        fill(sm);
+                        p.add(sm);
+                        sm = new JScrollMenu("Audio");
+                        fill(sm);
+                        p.add(sm);
+                        p.show(panel, e.getX(), e.getY());
+                    }
+
+                    private void fill(JScrollMenu p) {
                         int i = 1;
                         p.add(new JMenuItem("a " + (i++)));
                         p.add(new JMenuItem("a " + (i++)));
@@ -172,7 +185,7 @@ public class JScrollPopupMenu extends JPopupMenu {
 
                         p.add(new JMenuItem("a " + (i++)));
                         p.add(new JMenuItem("a " + (i++)));
-                        p.show(panel, e.getX(), e.getY());
+
                     }
                 });
             }
@@ -201,6 +214,17 @@ public class JScrollPopupMenu extends JPopupMenu {
     }
 
     private JScrollBar popupScrollBar;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
+    }
 
     protected JScrollBar getScrollBar() {
         if (popupScrollBar == null) {
@@ -285,8 +309,8 @@ public class JScrollPopupMenu extends JPopupMenu {
             int height = heightMargin + extent;
             if (isPreferredSizeSet()) {
                 Dimension pref = getPreferredSize();
-             height = Math.max(pref.height,height);
-               width= Math.max(pref.width,width);
+                height = Math.max(pref.height, height);
+                width = Math.max(pref.width, width);
             }
             setPopupSize(new Dimension(width, height));
         }
--- src/org/appwork/utils/Application.java
+++ src/org/appwork/utils/Application.java
@@ -72,7 +72,7 @@ import org.appwork.utils.os.CrossSystem;
  */
 public class Application {
 
-    private static Boolean              IS_JARED      = null;
+    private static Boolean IS_JARED = null;
 
     static {
         Application.redirectOutputStreams();
@@ -598,7 +598,7 @@ public class Application {
             org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().warning("Java 1.6 Update 18 has a serious bug in garbage collector!");
             /*
              * java 1.6 update 18 has a bug in garbage collector, causes java crashes
-             * 
+             *
              * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6847956
              */
             return true;
@@ -621,7 +621,7 @@ public class Application {
             org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().warning("freezing AppKit thread bug");
             /*
              * http://bugs.java.com/view_bug.do?bug_id=8025588
-             * 
+             *
              * Frozen AppKit thread
              */
             return true;
@@ -679,7 +679,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         * 
+         *
          * @see java.io.OutputStream#write(int)
          */
         @Override
@@ -703,7 +703,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         * 
+         *
          * @see java.io.OutputStream#write(byte[])
          */
         @Override
@@ -726,7 +726,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         * 
+         *
          * @see java.io.OutputStream#write(byte[], int, int)
          */
         @Override
@@ -750,7 +750,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         * 
+         *
          * @see java.io.OutputStream#flush()
          */
         @Override
@@ -773,7 +773,7 @@ public class Application {
 
         /*
          * (non-Javadoc)
-         * 
+         *
          * @see java.io.OutputStream#close()
          */
         @Override
@@ -939,12 +939,12 @@ public class Application {
             return ret;
         }
         try {
+
             final Class<? extends LookAndFeel> lafClass = UIManager.getLookAndFeel().getClass();
-            if (lafClass != null && lafClass.getName().startsWith("de.javasoft.plaf.synthetica")) {
-                // this loads the Synthetica class and triggers the license check. we should only do this if we are pretty sure
-                final Class<?> cls = Class.forName("de.javasoft.plaf.synthetica.SyntheticaLookAndFeel");
-                ret = cls != null && cls.isAssignableFrom(lafClass);
-            }
+            Class<?> cls = Class.forName("de.javasoft.plaf.synthetica.SyntheticaLookAndFeel", false, Application.class.getClassLoader());
+
+            ret = cls != null && cls.isAssignableFrom(lafClass);
+
         } catch (Throwable e) {
         } finally {
             if (ret != null) {

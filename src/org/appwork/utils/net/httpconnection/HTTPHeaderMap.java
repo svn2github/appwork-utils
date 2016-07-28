0f6cd2ede2243cb3d426d6e461ca365244eb9509
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-07-28 12:02:46 +0000

    HTTPHeaderMap: -fixed java 1.6 compatibility
    git-svn-id: svn://svn.appwork.org/utils@2711 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/net/httpconnection/HTTPHeaderMap.java
+++ src/org/appwork/utils/net/httpconnection/HTTPHeaderMap.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -40,12 +40,11 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 /**
  * @author daniel
- * 
+ *
  */
 public class HTTPHeaderMap<V> implements Map<String, V> {
 
@@ -74,7 +73,9 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
                 @Override
                 public String next() {
                     final HTTPHeaderMapEntry next = it.next();
-                    if (next != null) { return next.getHttpHeaderFieldName(); }
+                    if (next != null) {
+                        return next.getHttpHeaderFieldName();
+                    }
                     return null;
                 }
 
@@ -108,9 +109,15 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
         @Override
         public boolean equals(final Object obj) {
-            if (obj == null) { return false; }
-            if (obj == this) { return true; }
-            if (!(obj instanceof HTTPHeaderMapEntry)) { return false; }
+            if (obj == null) {
+                return false;
+            }
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof HTTPHeaderMapEntry)) {
+                return false;
+            }
             final HTTPHeaderMapEntry other = (HTTPHeaderMapEntry) obj;
             return other.getHttpHeaderFieldName().equalsIgnoreCase(this.getHttpHeaderFieldName());
         }
@@ -190,7 +197,9 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
     @Override
     public boolean containsKey(final Object key) {
-        if (key != null && key instanceof String) { return this.internalMap.containsKey(new HTTPHeaderMapEntry((String) key)); }
+        if (key != null && key instanceof String) {
+            return this.internalMap.containsKey(new HTTPHeaderMapEntry((String) key));
+        }
         return false;
     }
 
@@ -211,14 +220,18 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
             @Override
             public final boolean equals(final Object o) {
-                if (!(o instanceof Map.Entry)) { return false; }
+                if (!(o instanceof Map.Entry)) {
+                    return false;
+                }
                 final Map.Entry e = (Map.Entry) o;
                 final Object k1 = this.getKey();
                 final Object k2 = e.getKey();
                 if (k1 == k2 || k1 != null && k1.equals(k2)) {
                     final Object v1 = this.getValue();
                     final Object v2 = e.getValue();
-                    if (v1 == v2 || v1 != null && v1.equals(v2)) { return true; }
+                    if (v1 == v2 || v1 != null && v1.equals(v2)) {
+                        return true;
+                    }
                 }
                 return false;
             }
@@ -235,7 +248,11 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
             @Override
             public final int hashCode() {
-                return Objects.hashCode(this.getKey()) ^ Objects.hashCode(this.getValue());
+                return hashCode(this.getKey()) ^ hashCode(this.getValue());
+            }
+
+            private int hashCode(Object o) {
+                return o != null ? o.hashCode() : 0;
             }
 
             @Override
@@ -265,14 +282,18 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
             @Override
             public final boolean equals(final Object o) {
-                if (!(o instanceof Map.Entry)) { return false; }
+                if (!(o instanceof Map.Entry)) {
+                    return false;
+                }
                 final Map.Entry e = (Map.Entry) o;
                 final Object k1 = this.getKey();
                 final Object k2 = e.getKey();
                 if (k1 == k2 || k1 != null && k1.equals(k2)) {
                     final Object v1 = this.getValue();
                     final Object v2 = e.getValue();
-                    if (v1 == v2 || v1 != null && v1.equals(v2)) { return true; }
+                    if (v1 == v2 || v1 != null && v1.equals(v2)) {
+                        return true;
+                    }
                 }
                 return false;
             }
@@ -289,7 +310,11 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
             @Override
             public final int hashCode() {
-                return Objects.hashCode(this.getKey()) ^ Objects.hashCode(this.getValue());
+                return hashCode(this.getKey()) ^ hashCode(this.getValue());
+            }
+
+            private int hashCode(Object o) {
+                return o != null ? o.hashCode() : 0;
             }
 
             @Override
@@ -341,17 +366,25 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
             @Override
             public boolean containsAll(final Collection<?> c) {
                 for (final Object o : c) {
-                    if (!this.contains(o)) { return false; }
+                    if (!this.contains(o)) {
+                        return false;
+                    }
                 }
                 return true;
             }
 
             @Override
             public boolean equals(final Object o) {
-                if (o == this) { return true; }
-                if (!(o instanceof Set)) { return false; }
+                if (o == this) {
+                    return true;
+                }
+                if (!(o instanceof Set)) {
+                    return false;
+                }
                 final Collection c = (Collection) o;
-                if (c.size() != this.size()) { return false; }
+                if (c.size() != this.size()) {
+                    return false;
+                }
                 try {
                     return this.containsAll(c);
                 } catch (final ClassCastException unused) {
@@ -362,15 +395,21 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
             }
 
             private Entry<HTTPHeaderMapEntry, V> get(final Object o) {
-                if (!(o instanceof Map.Entry)) { return null; }
+                if (!(o instanceof Map.Entry)) {
+                    return null;
+                }
                 final Map.Entry<?, V> e = (Map.Entry<?, V>) o;
-                if (e.getKey() != null && !(e.getKey() instanceof String)) { return null; }
+                if (e.getKey() != null && !(e.getKey() instanceof String)) {
+                    return null;
+                }
                 final Iterator<Entry<HTTPHeaderMapEntry, V>> it = HTTPHeaderMap.this.internalMap.entrySet().iterator();
                 while (it.hasNext()) {
                     final Entry<HTTPHeaderMapEntry, V> next = it.next();
                     final HTTPHeaderMapEntry key = next.getKey();
                     if (key == null && e.getKey() == null || key.getHttpHeaderFieldName().equalsIgnoreCase((String) e.getKey())) {
-                        if (next.getValue() == e.getKey() || next.getValue() != null && next.getValue().equals(e.getValue())) { return next; }
+                        if (next.getValue() == e.getKey() || next.getValue() != null && next.getValue().equals(e.getValue())) {
+                            return next;
+                        }
                     }
                 }
                 return null;
@@ -407,7 +446,9 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
                     @Override
                     public Map.Entry<String, V> next() {
                         final Entry<HTTPHeaderMapEntry, V> next = it.next();
-                        if (next != null) { return HTTPHeaderMap.this.convertFromEntry(next); }
+                        if (next != null) {
+                            return HTTPHeaderMap.this.convertFromEntry(next);
+                        }
                         return null;
                     }
 
@@ -421,7 +462,9 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
             @Override
             public boolean remove(final Object o) {
                 final Entry<HTTPHeaderMapEntry, V> entry = this.get(o);
-                if (entry != null) { return HTTPHeaderMap.this.internalMap.entrySet().remove(entry); }
+                if (entry != null) {
+                    return HTTPHeaderMap.this.internalMap.entrySet().remove(entry);
+                }
                 return false;
             }
 
@@ -483,8 +526,12 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
     @Override
     public V get(final Object key) {
-        if (key == null) { return this.internalMap.get(null); }
-        if (key instanceof String) { return this.internalMap.get(new HTTPHeaderMapEntry((String) key)); }
+        if (key == null) {
+            return this.internalMap.get(null);
+        }
+        if (key instanceof String) {
+            return this.internalMap.get(new HTTPHeaderMapEntry((String) key));
+        }
         return null;
     }
 
@@ -500,13 +547,17 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
     @Override
     public V put(final String key, final V value) {
-        if (key == null) { return this.internalMap.put(null, value); }
+        if (key == null) {
+            return this.internalMap.put(null, value);
+        }
         return this.internalMap.put(new HTTPHeaderMapEntry(key), value);
     }
 
     @Override
     public void putAll(final Map<? extends String, ? extends V> m) {
-        if (m.size() == 0) { return; }
+        if (m.size() == 0) {
+            return;
+        }
         for (final Map.Entry<? extends String, ? extends V> e : m.entrySet()) {
             this.put(e.getKey(), e.getValue());
         }
@@ -514,8 +565,12 @@ public class HTTPHeaderMap<V> implements Map<String, V> {
 
     @Override
     public V remove(final Object key) {
-        if (key == null) { return this.internalMap.remove(null); }
-        if (key instanceof String) { return this.internalMap.remove(new HTTPHeaderMapEntry((String) key)); }
+        if (key == null) {
+            return this.internalMap.remove(null);
+        }
+        if (key instanceof String) {
+            return this.internalMap.remove(new HTTPHeaderMapEntry((String) key));
+        }
         return null;
     }
 

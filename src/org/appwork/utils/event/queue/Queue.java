6c9ce796a7d109d33d6efdade161d7b3ecda5fa3
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-08-09 10:05:12 +0000

    QueueAction: -added new protected methods onEnqueu and preRun Queue: -updated to call QueueAction.onEnqueu
    git-svn-id: svn://svn.appwork.org/utils@2718 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/event/queue/Queue.java
+++ src/org/appwork/utils/event/queue/Queue.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -39,8 +39,6 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
-
-
 
 /**
  * @author daniel
@@ -99,6 +97,7 @@ public abstract class Queue {
         /* set calling Thread to current item */
         action.reset();
         action.setCallerThread(this, Thread.currentThread());
+        action.onEnqueu(this);
         if (this.isQueueThread(action)) {
             /*
              * call comes from current running item, so lets start item
@@ -133,6 +132,7 @@ public abstract class Queue {
             this.addStats.incrementAndGet();
             action.reset();
             action.setCallerThread(this, Thread.currentThread());
+            action.onEnqueu(this);
             this.internalAdd(action);
         }
     }
@@ -142,6 +142,7 @@ public abstract class Queue {
         /* set calling Thread to current item */
         item.reset();
         item.setCallerThread(this, Thread.currentThread());
+        item.onEnqueu(this);
         if (this.isQueueThread(item)) {
             /*
              * call comes from current running item, so lets start item excaption handling is passed to top item. startItem throws an
@@ -191,6 +192,7 @@ public abstract class Queue {
         /* set calling Thread to current item */
         action.reset();
         action.setCallerThread(this, Thread.currentThread());
+        action.onEnqueu(this);
         this.internalAdd(action);
     }
 
@@ -246,7 +248,7 @@ public abstract class Queue {
 
     }
 
-    public void internalAdd(final QueueAction<?, ?> action) {
+    protected void internalAdd(final QueueAction<?, ?> action) {
         if (action != null) {
             synchronized (this.queueLock) {
                 try {
@@ -427,7 +429,7 @@ public abstract class Queue {
                         this.onItemHandled(item);
                     }
                 } catch (final Throwable e) {
-                          org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Queue rescued!");
+                    org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Queue rescued!");
                     org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().log(e);
                 }
             }
@@ -511,7 +513,7 @@ public abstract class Queue {
 
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see java.lang.Thread#toString()
      */
     @Override
--- src/org/appwork/utils/event/queue/QueueAction.java
+++ src/org/appwork/utils/event/queue/QueueAction.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -35,10 +35,9 @@ package org.appwork.utils.event.queue;
 
 import org.appwork.utils.event.queue.Queue.QueuePriority;
 
-
 /**
  * @author daniel
- * 
+ *
  */
 public abstract class QueueAction<T, E extends Throwable> {
 
@@ -71,7 +70,9 @@ public abstract class QueueAction<T, E extends Throwable> {
      * @return
      */
     protected synchronized boolean callExceptionHandler() {
-        if (this.exeption == null) { return true; }
+        if (this.exeption == null) {
+            return true;
+        }
         if (this.exeption != null && this.handleException(this.exeption)) {
             this.exeption = null;
             return true;
@@ -115,9 +116,8 @@ public abstract class QueueAction<T, E extends Throwable> {
     }
 
     /**
-     * Callback for asynchron queuecalls if exceptions occured. has to return
-     * true if exception got handled
-     * 
+     * Callback for asynchron queuecalls if exceptions occured. has to return true if exception got handled
+     *
      * @param e
      * @return
      */
@@ -132,14 +132,22 @@ public abstract class QueueAction<T, E extends Throwable> {
 
     public void kill() {
         synchronized (this) {
-            if (this.finished == true) { return; }
+            if (this.finished == true) {
+                return;
+            }
             this.killed = true;
             this.finished = true;
             this.notifyAll();
         }
     }
 
+    protected void onEnqueu(Queue queue) {
+    }
+
     protected void postRun() {
+    }
+
+    protected void preRun() {
     }
 
     public void reset() {
@@ -186,11 +194,16 @@ public abstract class QueueAction<T, E extends Throwable> {
         this.queue = queue;
         this.started = true;
         try {
+            this.preRun();
+        } catch (final Throwable e) {
+            org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().log(e);
+        }
+        try {
             this.result = this.run();
         } catch (final Throwable th) {
 
             if (queue != null && queue.isDebug()) {
-                      org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().severe("QueueActionCallerStackTrace:\r\n" + this.callerStackTrace);
+                org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().severe("QueueActionCallerStackTrace:\r\n" + this.callerStackTrace);
             }
             this.exeption = th;
             if (th instanceof RuntimeException) {

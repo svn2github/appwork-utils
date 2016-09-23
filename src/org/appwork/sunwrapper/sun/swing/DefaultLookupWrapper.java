8333e5415d1960df28f3c35d35b5c3586add874f
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-09-23 16:21:23 +0000

    Updated Sun/Swing Wrapper build>full
    git-svn-id: svn://svn.appwork.org/utils@2757 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/sunwrapper/sun/swing/DefaultLookupWrapper.java
+++ src/org/appwork/sunwrapper/sun/swing/DefaultLookupWrapper.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -44,10 +44,9 @@ import org.appwork.utils.logging2.extmanager.LoggerFactory;
 
 /**
  * @author Thomas
- * 
+ *
  */
 public class DefaultLookupWrapper {
-
     /**
      * @param extTableHeaderRenderer
      * @param ui
@@ -59,13 +58,10 @@ public class DefaultLookupWrapper {
             return sun.swing.DefaultLookup.getColor(comp, ui, key);
         } catch (final NoClassDefFoundError e) {
             LoggerFactory.I().getLogger(DefaultLookupWrapper.class.getName()).log(e);
-            // DefaultLookupWrapper is sun.swing, any may not be
-            // available
-            // e.gh. in 1.6.0_01-b06
-            return (Color) UIManager.get("TableHeader.focusCellForeground", comp.getLocale());
-
+        } catch (final IllegalAccessError e) {
+            LoggerFactory.I().getLogger(DefaultLookupWrapper.class.getName()).log(e);
         }
-
+        return (Color) UIManager.get("TableHeader.focusCellForeground", comp.getLocale());
     }
 
     /**
@@ -77,14 +73,11 @@ public class DefaultLookupWrapper {
     public static Border getBorder(JComponent comp, ComponentUI ui, String key) {
         try {
             return sun.swing.DefaultLookup.getBorder(comp, ui, key);
-
         } catch (final NoClassDefFoundError e) {
-            // DefaultLookupWrapper is sun.swing, any may not be available
-            // e.gh. in 1.6.0_01-b06
-
-            return (Border) UIManager.get("TableHeader.focusCellBorder", comp.getLocale());
-
+            LoggerFactory.I().getLogger(DefaultLookupWrapper.class.getName()).log(e);
+        } catch (final IllegalAccessError e) {
+            LoggerFactory.I().getLogger(DefaultLookupWrapper.class.getName()).log(e);
         }
+        return (Border) UIManager.get("TableHeader.focusCellBorder", comp.getLocale());
     }
-
 }
--- src/org/appwork/sunwrapper/sun/swing/SwingUtilities2Wrapper.java
+++ src/org/appwork/sunwrapper/sun/swing/SwingUtilities2Wrapper.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -42,10 +42,9 @@ import org.appwork.utils.logging2.extmanager.LoggerFactory;
 
 /**
  * @author Thomas
- * 
+ *
  */
 public class SwingUtilities2Wrapper {
-
     private static boolean CLIP_STRING_IF_NECESSARY_OK = true;
 
     /**
@@ -57,13 +56,15 @@ public class SwingUtilities2Wrapper {
      */
     public static String clipStringIfNecessary(JComponent component, FontMetrics fontMetrics, String str, int availableWidth) {
         try {
-            if (CLIP_STRING_IF_NECESSARY_OK) { return sun.swing.SwingUtilities2.clipStringIfNecessary(component, fontMetrics, str, availableWidth);
-
+            if (CLIP_STRING_IF_NECESSARY_OK) {
+                return sun.swing.SwingUtilities2.clipStringIfNecessary(component, fontMetrics, str, availableWidth);
             }
-        } catch (NoClassDefFoundError e) {
+        } catch (final NoClassDefFoundError e) {
             CLIP_STRING_IF_NECESSARY_OK = false;
             LoggerFactory.I().getLogger(SwingUtilities2Wrapper.class.getName()).log(e);
-
+        } catch (final IllegalAccessError e) {
+            CLIP_STRING_IF_NECESSARY_OK = false;
+            LoggerFactory.I().getLogger(SwingUtilities2Wrapper.class.getName()).log(e);
         }
         System.err.println("sun.swing.SwingUtilities2.clipStringIfNecessary failed");
         return str;
@@ -76,12 +77,10 @@ public class SwingUtilities2Wrapper {
     public static void setSkipClickCount(Component dispatchComponent, int i) {
         try {
             sun.swing.SwingUtilities2.setSkipClickCount(dispatchComponent, i);
-
-        } catch (NoClassDefFoundError e) {
-
+        } catch (final NoClassDefFoundError e) {
             LoggerFactory.I().getLogger(SwingUtilities2Wrapper.class.getName()).log(e);
-
+        } catch (final IllegalAccessError e) {
+            LoggerFactory.I().getLogger(SwingUtilities2Wrapper.class.getName()).log(e);
         }
     }
-
 }

6b3c1e1ed4b1df36c29a685154af0602874b40c6
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2016-10-13 15:39:01 +0000

    git-svn-id: svn://svn.appwork.org/utils@2775 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/os/CrossSystem.java
+++ src/org/appwork/utils/os/CrossSystem.java
@@ -362,7 +362,7 @@ public class CrossSystem {
         pathPart = pathPart.replaceFirst("^\\.+", ".");
         /*
          * remove ending dots, not allowed under windows and others os maybe too
-         * 
+         *
          * Do not end a file or directory name with a space or a period.
          */
         pathPart = pathPart.replaceFirst("\\.+$", "");
@@ -400,7 +400,7 @@ public class CrossSystem {
             /* windows uses \ as path separator */
             final boolean network = path.startsWith("\\\\");
             path = path.replaceAll("(/+)", "\\\\");
-            path = path.replaceAll("(\\\\)+", "\\\\");
+            path = path.replaceAll("(\\\\+)", "\\\\");
             if (network) {
                 path = "\\" + path;
             }

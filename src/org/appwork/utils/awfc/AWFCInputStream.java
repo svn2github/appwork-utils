6db9e07b55b878532683bc6cea7df0537a7ae342
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2017-03-22 11:28:41 +0000

    git-svn-id: svn://svn.appwork.org/utils@2856 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/awfc/AWFCInputStream.java
+++ src/org/appwork/utils/awfc/AWFCInputStream.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -44,28 +44,24 @@ import org.appwork.utils.net.LimitedInputStream;
 
 /**
  * @author Daniel Wilhelm
- * 
+ *
  */
 public class AWFCInputStream extends InputStream {
-
     private final InputStream  is;
-    private LimitedInputStream lis              = null;
-    private MessageDigest      md               = null;
-    private boolean            headerRead       = false;
-    private AWFCEntryOptions   currentEntry     = null;
-    private byte[]             currentEntryHash = null;
-    private final byte[]       skipBuffer       = new byte[32767];
+    private LimitedInputStream lis          = null;
+    private MessageDigest      md           = null;
+    private boolean            headerRead   = false;
+    private AWFCEntryOptions   currentEntry = null;
+    private final byte[]       skipBuffer   = new byte[32767];
     private AWFCUtils          utils;
 
     public AWFCInputStream(final InputStream is) {
         this.is = is;
         this.utils = new AWFCUtils() {
-
             @Override
             public InputStream getCurrentInputStream() throws IOException {
                 return AWFCInputStream.this.getCurrentInputStream();
             }
-
         };
     }
 
@@ -79,9 +75,94 @@ public class AWFCInputStream extends InputStream {
         this.getCurrentInputStream().close();
     }
 
-    private synchronized InputStream getCurrentInputStream() throws IOException {
-        if (this.lis != null) { return this.lis; }
+    protected InputStream getInputStream() throws IOException {
         return this.is;
+    }
+
+    private synchronized InputStream getCurrentInputStream() throws IOException {
+        if (this.lis != null) {
+            return this.lis;
+        } else {
+            return this.is;
+        }
+    }
+
+    protected LimitedInputStream getLimitedInputStream(final AWFCEntryOptions entry) throws IOException {
+        if (entry != null) {
+            final MessageDigest md;
+            if (this.md != null) {
+                this.md.reset();
+                md = this.md;
+            } else {
+                md = null;
+            }
+            final long inputLimit = entry.getEntry().getSize();
+            final boolean hasPayLoad = entry.hasPayLoad();
+            return new LimitedInputStream(this.is, hasPayLoad ? inputLimit : 0) {
+                private byte[] hash = null;
+
+                @Override
+                public int available() throws IOException {
+                    final long ret = this.getLimit() - this.transferedBytes();
+                    if (ret > Integer.MAX_VALUE) {
+                        return Integer.MAX_VALUE;
+                    }
+                    return (int) ret;
+                }
+
+                @Override
+                public int read() throws IOException {
+                    final int ret = super.read();
+                    if (hasPayLoad && md != null) {
+                        if (ret != -1) {
+                            md.update((byte) ret);
+                        } else if (ret == -1) {
+                            if (hash == null) {
+                                hash = md.digest();
+                            }
+                            if (!Arrays.equals(hash, AWFCInputStream.this.currentEntry.getEntry().getHash())) {
+                                throw new IOException("Wrong hash for Entry: " + AWFCInputStream.this.currentEntry.getEntry());
+                            }
+                        }
+                    }
+                    return ret;
+                }
+
+                @Override
+                public int read(final byte[] b, final int off, final int len) throws IOException {
+                    final int ret = super.read(b, off, len);
+                    if (hasPayLoad && md != null) {
+                        if (ret > 0) {
+                            md.update(b, off, ret);
+                        } else if (ret == -1) {
+                            if (hash == null) {
+                                hash = md.digest();
+                            }
+                            if (!Arrays.equals(hash, AWFCInputStream.this.currentEntry.getEntry().getHash())) {
+                                throw new IOException("Wrong hash for Entry: " + AWFCInputStream.this.currentEntry.getEntry());
+                            }
+                        }
+                    }
+                    return ret;
+                }
+
+                @Override
+                public long skip(final long n) throws IOException {
+                    if (n < AWFCInputStream.this.skipBuffer.length) {
+                        return this.read(AWFCInputStream.this.skipBuffer, 0, (int) n);
+                    } else {
+                        return this.read(AWFCInputStream.this.skipBuffer, 0, this.skipBuffer.length);
+                    }
+                }
+            };
+        }
+        return null;
+    }
+
+    protected synchronized void skipEntry(AWFCEntryOptions entry) throws IOException {
+        while (this.lis.available() > 0) {
+            this.lis.skip(this.lis.available());
+        }
     }
 
     public synchronized AWFCEntry getNextEntry() throws IOException {
@@ -89,75 +170,20 @@ public class AWFCInputStream extends InputStream {
             this.readAWFCHeader();
         }
         if (this.currentEntry != null) {
-            while (this.lis.available() > 0) {
-                this.lis.skip(this.lis.available());
+            try {
+                skipEntry(currentEntry);
+            } finally {
+                this.lis = null;
+                this.currentEntry = null;
             }
-            this.lis = null;
-            this.currentEntry = null;
-            this.currentEntryHash = null;
         }
         this.currentEntry = this.readAWFCEntry();
-        if (this.currentEntry == null) { return null; }
-        if (this.md != null) {
-            this.md.reset();
+        if (this.currentEntry == null) {
+            return null;
+        } else {
+            this.lis = getLimitedInputStream(currentEntry);
+            return this.currentEntry.getEntry();
         }
-        long inputLimit = this.currentEntry.getEntry().getSize();
-        final boolean hasPayLoad = this.currentEntry.hasPayLoad();
-        if (hasPayLoad == false) {
-            inputLimit = 0;
-        }
-        this.lis = new LimitedInputStream(this.is, inputLimit) {
-
-            @Override
-            public int available() throws IOException {
-                final long ret = this.getLimit() - this.transferedBytes();
-                if (ret > Integer.MAX_VALUE) { return Integer.MAX_VALUE; }
-                return (int) ret;
-            }
-
-            @Override
-            public int read() throws IOException {
-                final int ret = super.read();
-                if (hasPayLoad && AWFCInputStream.this.md != null) {
-                    if (ret != -1) {
-                        AWFCInputStream.this.md.update((byte) ret);
-                    } else if (ret == -1) {
-                        if (AWFCInputStream.this.currentEntryHash == null) {
-                            AWFCInputStream.this.currentEntryHash = AWFCInputStream.this.md.digest();
-                        }
-                        if (!Arrays.equals(AWFCInputStream.this.currentEntryHash, AWFCInputStream.this.currentEntry.getEntry().getHash())) { throw new IOException("Wrong hash for Entry: " + AWFCInputStream.this.currentEntry.getEntry()); }
-                    }
-                }
-                return ret;
-            }
-
-            @Override
-            public int read(final byte[] b, final int off, final int len) throws IOException {
-                final int ret = super.read(b, off, len);
-                if (hasPayLoad && AWFCInputStream.this.md != null) {
-                    if (ret > 0) {
-                        AWFCInputStream.this.md.update(b, off, ret);
-                    } else if (ret == -1) {
-                        if (AWFCInputStream.this.currentEntryHash == null) {
-                            AWFCInputStream.this.currentEntryHash = AWFCInputStream.this.md.digest();
-                        }
-                        if (!Arrays.equals(AWFCInputStream.this.currentEntryHash, AWFCInputStream.this.currentEntry.getEntry().getHash())) { throw new IOException("Wrong hash for Entry: " + AWFCInputStream.this.currentEntry.getEntry()); }
-                    }
-                }
-                return ret;
-            }
-
-            @Override
-            public long skip(final long n) throws IOException {
-                if (n < AWFCInputStream.this.skipBuffer.length) {
-                    return this.read(AWFCInputStream.this.skipBuffer, 0, (int) n);
-                } else {
-                    return this.read(AWFCInputStream.this.skipBuffer, 0, this.skipBuffer.length);
-                }
-            }
-
-        };
-        return this.currentEntry.getEntry();
     }
 
     @Override
@@ -200,9 +226,11 @@ public class AWFCInputStream extends InputStream {
             entry = new AWFCEntry(path + "/", 0, null);
         } else {
             final long size = this.utils.readLongOptimized();
-            byte[] hash = null;
-            if (this.md != null) {
+            final byte[] hash;
+            if (this.md != null && size > 0) {
                 hash = this.utils.ensureRead(this.md.getDigestLength(), null);
+            } else {
+                hash = null;
             }
             entry = new AWFCEntry(path, size, hash);
         }
@@ -211,7 +239,9 @@ public class AWFCInputStream extends InputStream {
 
     private synchronized void readAWFCHeader() throws IOException {
         final int version = this.utils.ensureRead();
-        if (version != 1) { throw new IOException("Unknown AWFC Version " + version); }
+        if (version != 1) {
+            throw new IOException("Unknown AWFC Version " + version);
+        }
         if (this.utils.readBoolean()) {
             final int stringSize = this.utils.readShort();
             final String mdAlgo = this.utils.readString(stringSize);
@@ -220,7 +250,9 @@ public class AWFCInputStream extends InputStream {
             } catch (final NoSuchAlgorithmException e) {
                 throw new IOException(e);
             }
-            if (this.md.getDigestLength() != this.utils.readShort()) { throw new IOException("Hashlength does not match for given md: " + mdAlgo); }
+            if (this.md.getDigestLength() != this.utils.readShort()) {
+                throw new IOException("Hashlength does not match for given md: " + mdAlgo);
+            }
         }
         this.headerRead = true;
     }
@@ -233,5 +265,4 @@ public class AWFCInputStream extends InputStream {
     public long skip(final long n) throws IOException {
         return this.getCurrentInputStream().skip(n);
     }
-
 }

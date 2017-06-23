90835473ca47585963c3426b89f037d845f499ef
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2017-06-23 16:59:43 +0000

    build>full
    git-svn-id: svn://svn.appwork.org/utils@2929 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/os/CrossSystem.java
+++ src/org/appwork/utils/os/CrossSystem.java
@@ -199,6 +199,7 @@ public class CrossSystem {
                     }
                 }
                 if (armVx && revision != null) {
+                    System.out.println("Revision:" + revision);
                     if (revision.startsWith("1000") && revision.length() > 4) {
                         revision = revision.substring(4);
                     }
@@ -469,7 +470,7 @@ public class CrossSystem {
         pathPart = pathPart.replaceFirst("^\\.+", ".");
         /*
          * remove ending dots, not allowed under windows and others os maybe too
-         *
+         * 
          * Do not end a file or directory name with a space or a period.
          */
         pathPart = pathPart.replaceFirst("\\.+$", "");

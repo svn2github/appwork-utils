8d382e1aa5a777fe0b879739031efa05fa8b16fd
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2017-07-27 15:12:46 +0000

    git-svn-id: svn://svn.appwork.org/utils@2954 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/Hash.java
+++ src/org/appwork/utils/Hash.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -44,11 +44,9 @@ import java.util.zip.CheckedInputStream;
 import org.appwork.utils.formatter.HexFormatter;
 
 public class Hash {
-
     public static final String HASH_TYPE_SHA256 = "SHA-256";
-
+    public static final String HASH_TYPE_SHA512 = "SHA-512";
     public static String       HASH_TYPE_MD5    = "md5";
-
     public static String       HASH_TYPE_SHA1   = "SHA-1";
 
     /**
@@ -112,7 +110,9 @@ public class Hash {
     }
 
     public static byte[] getFileHashBytes(final File arg, final String type) {
-        if (arg == null || !arg.exists() || arg.isDirectory()) { return null; }
+        if (arg == null || !arg.exists() || arg.isDirectory()) {
+            return null;
+        }
         FileInputStream fis = null;
         MessageDigest md = null;
         try {
@@ -140,12 +140,16 @@ public class Hash {
 
     public static String getFileHash(final File arg, final String type) {
         byte[] bytes = Hash.getFileHashBytes(arg, type);
-        if (bytes == null) { return null; }
+        if (bytes == null) {
+            return null;
+        }
         return HexFormatter.byteArrayToHex(bytes);
     }
 
     public static String getFileHash(final File arg, final String type, final long maxHash) {
-        if (arg == null || !arg.exists() || arg.isDirectory()) { return null; }
+        if (arg == null || !arg.exists() || arg.isDirectory()) {
+            return null;
+        }
         FileInputStream fis = null;
         MessageDigest md = null;
         try {
@@ -172,7 +176,6 @@ public class Hash {
                         bufferSize = (int) todo;
                     }
                 }
-
             }
         } catch (final Throwable e) {
             e.printStackTrace();
@@ -245,5 +248,4 @@ public class Hash {
         }
         return null;
     }
-
 }

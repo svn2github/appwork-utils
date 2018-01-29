19758324d88ec568801cf2f75ea259b55101f46e
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2018-01-29 11:18:58 +0000

    ChunkedInputStream: -wrapNumberFormatException into IOException
    git-svn-id: svn://svn.appwork.org/utils@3023 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/net/ChunkedInputStream.java
+++ src/org/appwork/utils/net/ChunkedInputStream.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,9 +25,9 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
@@ -39,10 +39,9 @@ import java.io.InputStream;
 
 /**
  * @author daniel, ChunkedInputStream, see rfc2616#section-3.6
- * 
+ *
  */
 public class ChunkedInputStream extends InputStream implements StreamValidEOF {
-
     private final InputStream is;
     private volatile int      nextChunkSize = 0;
     private volatile int      nextChunkLeft = 0;
@@ -55,20 +54,26 @@ public class ChunkedInputStream extends InputStream implements StreamValidEOF {
 
     @Override
     public int available() throws IOException {
-        if (this.nextChunkLeft > 0) { return this.nextChunkLeft; }
-        return this.is.available();
+        if (this.nextChunkLeft > 0) {
+            return this.nextChunkLeft;
+        } else {
+            return this.is.available();
+        }
     }
 
     /**
-     * returns available bytes in current Chunk or reads next Chunk and parses
-     * it
-     * 
+     * returns available bytes in current Chunk or reads next Chunk and parses it
+     *
      * @return
      * @throws IOException
      */
     private int availableChunkData() throws IOException {
-        if (this.nextChunkLeft == -1) { return -1; }
-        if (this.nextChunkLeft > 0) { return this.nextChunkLeft; }
+        if (this.nextChunkLeft == -1) {
+            return -1;
+        }
+        if (this.nextChunkLeft > 0) {
+            return this.nextChunkLeft;
+        }
         final StringBuilder sb = new StringBuilder();
         boolean chunkExt = false;
         final byte[] b = { (byte) 0x00 };
@@ -93,14 +98,20 @@ public class ChunkedInputStream extends InputStream implements StreamValidEOF {
             }
             read = this.is.read();
         }
-        if (read == -1 && sb.length() == 0) { return -1; }
+        if (read == -1 && sb.length() == 0) {
+            return -1;
+        }
         if (read == 13) {
             /* finish CRLF here */
             read = this.is.read();
         }
         this.nextChunkSize = 0;
         if (sb.length() > 0) {
-            this.nextChunkSize = Integer.parseInt(sb.toString().trim(), 16);
+            try {
+                this.nextChunkSize = Integer.parseInt(sb.toString().trim(), 16);
+            } catch (NumberFormatException e) {
+                throw new IOException(e);
+            }
         }
         if (this.nextChunkSize == 0) {
             // System.out.println("lastChunk");
@@ -121,7 +132,7 @@ public class ChunkedInputStream extends InputStream implements StreamValidEOF {
 
     /**
      * Exhaust an input stream, reading until EOF has been encountered.
-     * 
+     *
      * @throws IOException
      */
     private void exhaustInputStream() throws IOException {
@@ -162,10 +173,12 @@ public class ChunkedInputStream extends InputStream implements StreamValidEOF {
             if (ret != -1) {
                 this.nextChunkLeft--;
                 return ret;
+            } else {
+                throw new EOFException("premature EOF");
             }
-            throw new EOFException("premature EOF");
+        } else {
+            return -1;
         }
-        return -1;
     }
 
     @Override
@@ -176,15 +189,17 @@ public class ChunkedInputStream extends InputStream implements StreamValidEOF {
             if (ret != -1) {
                 this.nextChunkLeft -= ret;
                 return ret;
+            } else {
+                throw new EOFException("premature EOF");
             }
-            throw new EOFException("premature EOF");
+        } else {
+            return -1;
         }
-        return -1;
     }
 
     /**
      * TODO: read the Trailers we read until EOF at the moment;
-     * 
+     *
      * @throws IOException
      */
     private void readTrailers() throws IOException {
@@ -195,5 +210,4 @@ public class ChunkedInputStream extends InputStream implements StreamValidEOF {
     public boolean isValidEOF() {
         return this.EOF;
     }
-
 }

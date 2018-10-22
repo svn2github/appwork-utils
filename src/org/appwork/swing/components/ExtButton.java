fc6a810048c7f4b8a292b2fa3062180ad3da173a
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2018-10-22 13:10:17 +0000

    to get a uniform look and feel, buttons with icons (horizontal) should have the same height as buttons without an icon
    git-svn-id: svn://svn.appwork.org/utils@3159 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/ExtButton.java
+++ src/org/appwork/swing/components/ExtButton.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * ====================================================================================================================================================
  *         "AppWork Utilities" License
  *         The "AppWork Utilities" will be called [The Product] from now on.
@@ -7,16 +7,16 @@
  *         Copyright (c) 2009-2015, AppWork GmbH <e-mail@appwork.org>
  *         Schwabacher Straße 117
  *         90763 Fürth
- *         Germany   
+ *         Germany
  * === Preamble ===
  *     This license establishes the terms under which the [The Product] Source Code & Binary files may be used, copied, modified, distributed, and/or redistributed.
  *     The intent is that the AppWork GmbH is able to provide their utilities library for free to non-commercial projects whereas commercial usage is only permitted after obtaining a commercial license.
  *     These terms apply to all files that have the [The Product] License header (IN the file), a <filename>.license or <filename>.info (like mylib.jar.info) file that contains a reference to this license.
- * 	
+ *
  * === 3rd Party Licences ===
  *     Some parts of the [The Product] use or reference 3rd party libraries and classes. These parts may have different licensing conditions. Please check the *.license and *.info files of included libraries
- *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header. 	
- * 	
+ *     to ensure that they are compatible to your use-case. Further more, some *.java have their own license. In this case, they have their license terms in the java file header.
+ *
  * === Definition: Commercial Usage ===
  *     If anybody or any organization is generating income (directly or indirectly) by using [The Product] or if there's any commercial interest or aspect in what you are doing, we consider this as a commercial usage.
  *     If your use-case is neither strictly private nor strictly educational, it is commercial. If you are unsure whether your use-case is commercial or not, consider it as commercial or contact us.
@@ -25,14 +25,15 @@
  *     If you want to use [The Product] in a commercial way (see definition above), you have to obtain a paid license from AppWork GmbH.
  *     Contact AppWork for further details: <e-mail@appwork.org>
  * === Non-Commercial Usage ===
- *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the 
+ *     If there is no commercial usage (see definition above), you may use [The Product] under the terms of the
  *     "GNU Affero General Public License" (http://www.gnu.org/licenses/agpl-3.0.en.html).
- * 	
+ *
  *     If the AGPL does not fit your needs, please contact us. We'll find a solution.
  * ====================================================================================================================================================
  * ==================================================================================================================================================== */
 package org.appwork.swing.components;
 
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
@@ -45,6 +46,7 @@ import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
 
 import org.appwork.swing.action.BasicAction;
 import org.appwork.swing.components.tooltips.ExtTooltip;
@@ -54,20 +56,19 @@ import org.appwork.swing.components.tooltips.TooltipFactory;
 import org.appwork.swing.components.tooltips.TooltipTextDelegateFactory;
 import org.appwork.utils.KeyUtils;
 import org.appwork.utils.StringUtils;
-import org.appwork.utils.ImageProvider.ImageProvider;
 
 public class ExtButton extends JButton implements ToolTipHandler {
-
     /**
-     * 
+     *
      */
-    private static final long serialVersionUID = -7151290227825542967L;
+    private static final long serialVersionUID    = -7151290227825542967L;
     private TooltipFactory    tooltipFactory;
     private MouseAdapter      rollOverlistener;
     private KeyStroke         accelerator;
+    private int               calculatingPrefSize = 0;
 
     /**
-     * 
+     *
      */
     public ExtButton() {
         this(null);
@@ -76,32 +77,81 @@ public class ExtButton extends JButton implements ToolTipHandler {
 
     protected void actionPropertyChanged(final Action action, final String propertyName) {
         super.actionPropertyChanged(action, propertyName);
-
         if (propertyName == Action.ACCELERATOR_KEY) {
             setAccelerator((KeyStroke) action.getValue(Action.ACCELERATOR_KEY));
         }
     }
-/* (non-Javadoc)
- * @see javax.swing.AbstractButton#setEnabled(boolean)
- */
-@Override
-public void setEnabled(final boolean b) {
-    // TODO Auto-generated method stub
-    super.setEnabled(b);
-}
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.JComponent#getPreferredSize()
+     */
+    @Override
+    public Dimension getPreferredSize() {
+        if (getIcon() == null || getVerticalTextPosition() != SwingConstants.CENTER) {
+            return super.getPreferredSize();
+        }
+        // buttons with icons should have the same height as buttons without icons.
+        calculatingPrefSize++;
+        Dimension withoutIcon;
+        try {
+            withoutIcon = super.getPreferredSize();
+            if (super.getIcon() == null) {
+                return withoutIcon;
+            }
+        } finally {
+            calculatingPrefSize--;
+        }
+        Dimension withIcon = super.getPreferredSize();
+        withIcon.height = withoutIcon.height;
+        return withIcon;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.JComponent#getMinimumSize()
+     */
+    @Override
+    public Dimension getMinimumSize() {
+        return getPreferredSize();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.AbstractButton#getIcon()
+     */
+    @Override
+    public Icon getIcon() {
+        if (calculatingPrefSize > 0) {
+            return null;
+        }
+        return super.getIcon();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.AbstractButton#setEnabled(boolean)
+     */
+    @Override
+    public void setEnabled(final boolean b) {
+        // TODO Auto-generated method stub
+        super.setEnabled(b);
+    }
+
     /**
      * @param autoDetectAction
      */
     public ExtButton(final AbstractAction action) {
         super(action);
         tooltipFactory = new TooltipTextDelegateFactory(this);
-
         if (!StringUtils.isEmpty(getToolTipText())) {
             setTooltipsEnabled(true);
         }
-
         if (action instanceof BasicAction) {
-
             if (((BasicAction) action).getTooltipFactory() != null) {
                 tooltipFactory = ((BasicAction) action).getTooltipFactory();
                 setTooltipsEnabled(true);
@@ -109,36 +159,26 @@ public void setEnabled(final boolean b) {
             if (!StringUtils.isEmpty(((BasicAction) action).getTooltipText())) {
                 setTooltipsEnabled(true);
             }
-
             setAccelerator((KeyStroke) action.getValue(Action.ACCELERATOR_KEY));
         }
-
     }
 
     public void setAccelerator(final KeyStroke newAccelerator) {
-
         final InputMap inputmap = getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW);
         if (accelerator != null) {
             inputmap.remove(newAccelerator);
-          
             getActionMap().remove(KeyUtils.getShortcutString(accelerator, true));
             accelerator = null;
             setTooltipsEnabled(StringUtils.isNotEmpty(getToolTipText()));
-        
-
         }
         if (newAccelerator != null) {
             accelerator = newAccelerator;
-
             final String shortcutString = KeyUtils.getShortcutString(newAccelerator, true);
             inputmap.put(newAccelerator, shortcutString);
-
             setTooltipsEnabled(true);
             getActionMap().put(shortcutString, new AbstractAction() {
-
                 @Override
                 public void actionPerformed(final ActionEvent e) {
-
                     doClick();
                 }
             });
@@ -147,22 +187,20 @@ public void setEnabled(final boolean b) {
 
     /*
      * (non-Javadoc)
-     * 
-     * @see
-     * org.appwork.swing.components.tooltips.ToolTipHandler#createExtTooltip
-     * (java.awt.Point)
+     *
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler#createExtTooltip (java.awt.Point)
      */
     @Override
     public ExtTooltip createExtTooltip(final Point mousePosition) {
-
         return getTooltipFactory().createTooltip();
     }
 
     public TooltipFactory getTooltipFactory() {
-
         if (getAction() instanceof BasicAction) {
             final TooltipFactory ret = ((BasicAction) getAction()).getTooltipFactory();
-            if (ret != null) { return ret; }
+            if (ret != null) {
+                return ret;
+            }
         }
         return tooltipFactory;
     }
@@ -175,7 +213,6 @@ public void setEnabled(final boolean b) {
                 ret = ((BasicAction) getAction()).getTooltipText();
             }
         }
-
         if (accelerator != null) {
             if (ret == null) {
                 ret = "";
@@ -185,20 +222,16 @@ public void setEnabled(final boolean b) {
             }
             ret += "[" + KeyUtils.getShortcutString(accelerator, true) + "]";
         }
-
         return ret;
-
     }
 
     /*
      * (non-Javadoc)
-     * 
-     * @see org.appwork.swing.components.tooltips.ToolTipHandler#
-     * isTooltipDisabledUntilNextRefocus()
+     *
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler# isTooltipDisabledUntilNextRefocus()
      */
     @Override
     public boolean isTooltipDisabledUntilNextRefocus() {
-
         return true;
     }
 
@@ -209,19 +242,17 @@ public void setEnabled(final boolean b) {
     }
 
     /**
-     * 
+     *
      */
     protected void onRollOut() {
         setContentAreaFilled(false);
-
     }
 
     /**
-     * 
+     *
      */
     protected void onRollOver() {
         setContentAreaFilled(true);
-
     }
 
     /**
@@ -231,31 +262,29 @@ public void setEnabled(final boolean b) {
         if (b) {
             if (rollOverlistener == null) {
                 rollOverlistener = new MouseAdapter() {
-
                     @Override
                     public void mouseEntered(final MouseEvent e) {
                         ExtButton.this.onRollOver();
-
                     }
 
                     @Override
                     public void mouseExited(final MouseEvent e) {
                         ExtButton.this.onRollOut();
-
                     }
-                    /* (non-Javadoc)
+
+                    /*
+                     * (non-Javadoc)
+                     *
                      * @see java.awt.event.MouseAdapter#mouseReleased(java.awt.event.MouseEvent)
                      */
                     @Override
                     public void mouseReleased(final MouseEvent e) {
                         ExtButton.this.onReleased();
                     }
-
                 };
             }
             addMouseListener(rollOverlistener);
             onRollOut();
-
         } else {
             if (rollOverlistener != null) {
                 removeMouseListener(rollOverlistener);
@@ -263,23 +292,24 @@ public void setEnabled(final boolean b) {
             }
         }
     }
-    private Icon disabledIcon=null;
+
+    private Icon disabledIcon = null;
+
     public Icon getDisabledIcon() {
         if (disabledIcon == null) {
-           
-            disabledIcon =  org.appwork.resources.AWUTheme.I().getDisabledIcon(getIcon());
+            disabledIcon = org.appwork.resources.AWUTheme.I().getDisabledIcon(getIcon());
             if (disabledIcon != null) {
                 firePropertyChange(DISABLED_ICON_CHANGED_PROPERTY, null, disabledIcon);
             }
         }
         return disabledIcon;
     }
+
     /**
-     * 
+     *
      */
     protected void onReleased() {
         // TODO Auto-generated method stub
-        
     }
 
     public void setTooltipFactory(final TooltipFactory tooltipFactory) {
@@ -292,25 +322,21 @@ public void setEnabled(final boolean b) {
             ToolTipController.getInstance().register(this);
         } else {
             ToolTipController.getInstance().unregister(this);
-
         }
         return this;
     }
 
     @Override
     public void setToolTipText(final String text) {
-
         putClientProperty(JComponent.TOOL_TIP_TEXT_KEY, text);
-
         setTooltipsEnabled(StringUtils.isNotEmpty(getToolTipText()));
     }
 
     /*
      * (non-Javadoc)
-     * 
-     * @see
-     * org.appwork.swing.components.tooltips.ToolTipHandler#updateTooltip(org
-     * .appwork.swing.components.tooltips.ExtTooltip, java.awt.event.MouseEvent)
+     *
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler#updateTooltip(org .appwork.swing.components.tooltips.ExtTooltip,
+     * java.awt.event.MouseEvent)
      */
     @Override
     public int getTooltipDelay(final Point mousePositionOnScreen) {
@@ -321,12 +347,9 @@ public void setEnabled(final boolean b) {
     public boolean updateTooltip(final ExtTooltip activeToolTip, final MouseEvent e) {
         return false;
     }
-
     /*
      * (non-Javadoc)
-     * 
-     * @see
-     * org.appwork.swing.components.tooltips.ToolTipHandler#getTooltipDelay()
+     *
+     * @see org.appwork.swing.components.tooltips.ToolTipHandler#getTooltipDelay()
      */
-
 }

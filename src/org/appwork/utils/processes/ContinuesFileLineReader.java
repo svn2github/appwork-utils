d41c0bc240e800a3fdbe1cfcefea2904c3287730
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2018-10-26 13:22:08 +0000

    ContinuesFileLineReader: -fixed npe in closeAndFlush -use AtomicReference for current thread
    git-svn-id: svn://svn.appwork.org/utils@3164 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/processes/ContinuesFileLineReader.java
+++ src/org/appwork/utils/processes/ContinuesFileLineReader.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.appwork.utils.net.LineParsingInputStream;
 import org.appwork.utils.net.LineParsingOutputStream.NEWLINE;
@@ -18,11 +19,11 @@ import org.appwork.utils.net.LineParsingOutputStream.NEWLINE;
  *
  */
 public class ContinuesFileLineReader {
-    protected final File          file;
-    protected final AtomicBoolean closedFlag = new AtomicBoolean(false);
-    protected Thread              thread     = null;
-    protected final LineHandler   sink;
-    protected final Charset       charset;
+    protected final File              file;
+    protected final AtomicBoolean     closedFlag = new AtomicBoolean(false);
+    protected AtomicReference<Thread> thread     = new AtomicReference<Thread>(null);
+    protected final LineHandler       sink;
+    protected final Charset           charset;
 
     public ContinuesFileLineReader(LineHandler sink, String path, Charset charset) {
         if (sink == null) {
@@ -41,62 +42,68 @@ public class ContinuesFileLineReader {
     protected volatile boolean   started = false;
 
     public synchronized ContinuesFileLineReader run() {
+        Thread thread = this.thread.get();
         if (thread != null && thread.isAlive()) {
             return this;
         }
         closedFlag.compareAndSet(true, false);
         exceptionIOException = null;
-        this.thread = new Thread("Read " + this.file) {
+        thread = new Thread("Read " + this.file) {
             @Override
             public void run() {
-                // Wait until the logfile is available
-                while (!ContinuesFileLineReader.this.file.exists() && !isClosed()) {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        // we leave the thread, no reason to reset the interrupt flag
-                        return;
-                    }
-                }
-                started = true;
-                if (!isClosed()) {
-                    try {
-                        final FileInputStream fis = new FileInputStream(file);
+                try {
+                    // Wait until the logfile is available
+                    while (!ContinuesFileLineReader.this.file.exists() && !isClosed()) {
                         try {
-                            final LineParsingInputStream is = new LineParsingInputStream(fis, charset) {
-                                @Override
-                                protected void onNextLine(NEWLINE newLine, long line, StringBuilder sb, int startIndex, int endIndex) {
-                                    ContinuesFileLineReader.this.onNextLine(newLine, line, sb, startIndex, endIndex);
-                                };
-                            };
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            // we leave the thread, no reason to reset the interrupt flag
+                            return;
+                        }
+                    }
+                    started = true;
+                    if (!isClosed()) {
+                        try {
+                            final FileInputStream fis = new FileInputStream(file);
                             try {
-                                final byte[] buf = new byte[32767];
-                                while (!isClosed()) {
-                                    final int read = is.read(buf);
-                                    if (read <= 0) {
-                                        if (isClosed()) {
-                                            break;
-                                        } else {
-                                            onWait(read, is.getLines());
+                                final LineParsingInputStream is = new LineParsingInputStream(fis, charset) {
+                                    @Override
+                                    protected void onNextLine(NEWLINE newLine, long line, StringBuilder sb, int startIndex, int endIndex) {
+                                        ContinuesFileLineReader.this.onNextLine(newLine, line, sb, startIndex, endIndex);
+                                    };
+                                };
+                                try {
+                                    final byte[] buf = new byte[32767];
+                                    while (!isClosed()) {
+                                        final int read = is.read(buf);
+                                        if (read <= 0) {
+                                            if (isClosed()) {
+                                                break;
+                                            } else {
+                                                onWait(read, is.getLines());
+                                            }
                                         }
                                     }
+                                } finally {
+                                    is.close();
                                 }
                             } finally {
-                                is.close();
+                                fis.close();
                             }
-                        } finally {
-                            fis.close();
+                        } catch (IOException e) {
+                            exceptionIOException = e;
+                        } catch (InterruptedException e) {
+                            // we leave the thread, no reason to reset the interrupt flag
+                            return;
                         }
-                    } catch (IOException e) {
-                        exceptionIOException = e;
-                    } catch (InterruptedException e) {
-                        // we leave the thread, no reason to reset the interrupt flag
-                        return;
                     }
+                } finally {
+                    ContinuesFileLineReader.this.thread.compareAndSet(Thread.currentThread(), null);
                 }
             }
         };
-        this.thread.start();
+        this.thread.set(thread);
+        thread.start();
         return this;
     }
 
@@ -105,19 +112,26 @@ public class ContinuesFileLineReader {
     }
 
     public void closeAndFlush() throws InterruptedException, IOException {
+        final Thread thread = this.thread.get();
         try {
             if (closedFlag.compareAndSet(false, true)) {
                 if (!started) {
                     // not started. file is not available
-                    thread.interrupt();
+                    if (thread != null) {
+                        thread.interrupt();
+                    }
                 }
-                this.thread.join();
+                if (thread != null) {
+                    thread.join();
+                }
                 if (exceptionIOException != null) {
                     throw exceptionIOException;
                 }
             }
         } catch (InterruptedException e) {
-            this.thread.interrupt();
+            if (thread != null) {
+                thread.interrupt();
+            }
             throw e;
         }
     }
